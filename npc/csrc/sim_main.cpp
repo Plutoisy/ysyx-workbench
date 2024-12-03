bad5787c5ac5bfe6bb2c0ebc92aac8281a8dbe55
tracer-ysyx <tracer@ysyx.org>
2024-12-03 17:52:26 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:52:26 up  2:14,  1 user,  load average: 0.00, 0.01, 0.04

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -1,6 +1,6 @@
 #include "Vtop.h"
 #include "verilated.h"
-#include "verilated_fst_c.h"
+#include "verilated_vcd_c.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -9,13 +9,13 @@ int main(int argc, char** argv) {
     contextp->commandArgs(argc, argv);
     Vtop* top = new Vtop{contextp};
     
-    VerilatedFstC* tfp = new VerilatedFstC;
+    VerilatedVcdC *tfp= new VerilatedVcdC;
     Verilated::traceEverOn(true);  
     top->trace(tfp, 99);           
-    tfp->open("./waveform/waveform.fst");     
+    tfp->open("waveform.vcd");     
     
     while (!contextp->gotFinish()) {
-    	contextp->timeInc(1);
+    	
     	int a = rand() & 1;
   	int b = rand() & 1; 
   	top->a = a;
@@ -23,6 +23,7 @@ int main(int argc, char** argv) {
     	top->eval(); 
     	printf("a = %d, b = %d, f = %d\n", a, b, top->f);
   	assert(top->f == (a ^ b));
+  	contextp->timeInc(1);
   	if (contextp->time() >= 60)
         {
             break;
