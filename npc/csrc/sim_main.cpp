ec164771739b7e9ffbd38229d5459dc1ae822b60
tracer-ysyx <tracer@ysyx.org>
2024-12-03 17:37:57 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:37:57 up  2:00,  1 user,  load average: 0.02, 0.05, 0.11

---
+++ npc/csrc/sim_main.cpp
@@ -0,0 +1,36 @@
+#include "Vtop.h"
+#include "verilated.h"
+#include "verilated_fst_c.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+int main(int argc, char** argv) {
+    VerilatedContext* contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+    Vtop* top = new Vtop{contextp};
+    
+    VerilatedFstC* tfp = new VerilatedFstC;
+    Verilated::traceEverOn(true);  
+    top->trace(tfp, 99);           
+    tfp->open("waveform.fst");     
+    
+    while (!contextp->gotFinish()) {
+    	contextp->timeInc(1);
+    	int a = rand() & 1;
+  	int b = rand() & 1; 
+  	top->a = a;
+  	top->b = b;
+    	top->eval(); 
+    	printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+  	assert(top->f == (a ^ b));
+  	if (contextp->time() >= 60)
+        {
+            break;
+        }
+        tfp->dump(contextp->time()); // dump wave
+
+    }
+    delete top;
+    delete contextp;
+    return 0;
+}
