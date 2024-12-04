3eff69fa115ecb564def570bf9478416880713cd
tracer-ysyx <tracer@ysyx.org>
2024-12-04 13:18:42 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:18:42 up  9:02,  1 user,  load average: 1.11, 0.78, 0.39

---
+++ npc/csrc/sim_main_light_NVBoard.cpp
@@ -0,0 +1,29 @@
+#include "Vtop.h"
+#include <nvboard.h>
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(TOP_NAME* top);
+
+void single_cycle() {
+  dut->clk = 0; dut->eval();
+  dut->clk = 1; dut->eval();
+}
+
+void reset(int n) {
+  dut->rst = 1;
+  while (n -- > 0) single_cycle();
+  dut->rst = 0;
+}
+
+int main(int argc, char** argv) {
+
+    nvboard_bind_all_pins(&dut);
+    nvboard_init();
+    reset(10);
+    while (1) {
+        single_cycle();
+    	nvboard_update();
+    }
+    nvboard_quit();
+    return 0;
+}
