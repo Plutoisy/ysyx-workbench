e946d64371673214f9e1b15b88983e933cf6c40b
tracer-ysyx <tracer@ysyx.org>
2024-12-07 21:09:08 +0800

    > compile NEMU ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:09:08 up 2 days, 14:55,  1 user,  load average: 0.24, 0.43, 0.31

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -49,11 +49,24 @@ static int cmd_c(char *args) {
 
 
 static int cmd_q(char *args) {
-  nemu_state.state = NEMU_QUIT;
+  nemu_state.state = NEMU_QUIT;//优美地退出
   return -1;
 }
 
 static int cmd_help(char *args);
+
+static int cmd_si(char *args) {
+  int N;
+  if (args == NULL){
+    N = 1;
+  }
+  else{
+    sscanf(args,"%d",&N);
+    //printf("%d\n",N);
+  }
+  cpu_exec(N);
+  return 0;
+}
 
 static struct {
   const char *name;
@@ -63,6 +76,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si","Execute one time", cmd_si},
 
   /* TODO: Add more commands */
 
