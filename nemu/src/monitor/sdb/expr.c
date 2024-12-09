9de3fceecfdb8f6b115a0703d15aaf4df87a0dac
tracer-ysyx <tracer@ysyx.org>
2024-12-09 13:43:20 +0800

    > compile NEMU ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:43:20 up 3 days,  5:03,  1 user,  load average: 0.26, 0.31, 0.33

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -398,6 +398,7 @@ word_t eval(int p, int q){
       case '*': return val1 * val2;
       case '/': return val1 / val2;
       case TK_EQ: return val1 == val2;
+      case TK_NOTEQ: return val1 != val2;
       case TK_MORETHAN: return val1 >= val2;
       case TK_LESSTHAN: return val1 <= val2;
       case TK_AND: return val1 && val2;
