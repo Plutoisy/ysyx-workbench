b16e7c1003cc2e34d9f948c17fd2885744f8ffcc
tracer-ysyx <tracer@ysyx.org>
2024-12-10 21:06:03 +0800

    > compile NEMU ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:06:03 up  9:04,  1 user,  load average: 0.01, 0.12, 0.23

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -39,7 +39,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
-  update_wp_value();
+  IFDEF(CONFIG_WATCHPOINT, update_wp_value());
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
