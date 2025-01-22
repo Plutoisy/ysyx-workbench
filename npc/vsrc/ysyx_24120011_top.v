f54e8d333b128766bfd23cfce2f551c0d280a995
tracer-ysyx <tracer@ysyx.org>
2025-01-22 15:45:42 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:45:42 up 2 days, 15:37,  1 user,  load average: 0.06, 0.48, 0.38

--- npc/vsrc/ysyx_24120011_top.v
+++ npc/vsrc/ysyx_24120011_top.v
@@ -2,8 +2,8 @@ module ysyx_24120011_top (
     input clk,
     input rst,
     input [31:0] inst,
-    output [31:0] pc
-    output [31:0] inst_out;
+    output [31:0] pc,
+    output [31:0] inst_out
 );
 
 ysyx_24120011_Reg #(32, 32'h8000_0000) i_pc (clk, rst, dnpc, pc, 1'b1);
