206e87fe79e79e92d16df090e7153efef5755012
tracer-ysyx <tracer@ysyx.org>
2025-01-25 16:51:04 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:51:04 up 3 days, 19:51,  1 user,  load average: 0.15, 0.05, 0.08

--- npc/vsrc/ysyx_24120011_ImmeGen.v
+++ npc/vsrc/ysyx_24120011_ImmeGen.v
@@ -6,7 +6,7 @@ module ysyx_24120011_ImmeGen(
 always@(*)begin
     case(opcode_type)
         3'd0      : imme = {{20{inst[31]}},inst[31:20]};//I-Type
-        default   : imme = inst;//Unknown
+        default   : imme = 32'h0000_0000;//Unknown
     endcase
 end
 endmodule
