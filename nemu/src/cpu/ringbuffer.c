146d7dd5f569d7b6a62ae8a05d8e5079b7162203
tracer-ysyx <tracer@ysyx.org>
2025-01-27 22:41:10 +0800

    > compile NEMU ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:41:10 up 14:19,  1 user,  load average: 0.19, 0.22, 0.29

--- nemu/src/cpu/ringbuffer.c
+++ nemu/src/cpu/ringbuffer.c
@@ -48,7 +48,6 @@ void destroy_ringbuffer(ringbuffer *rb){
 
 void print_ringbuffer(ringbuffer *rb){
 	Node *temp = rb->head;
-    printf("%d",rb->count);
 	for (int i = 0; i < rb->count; i++) {
 		if(i == rb->count - 1) {
 			printf("  --> ");
