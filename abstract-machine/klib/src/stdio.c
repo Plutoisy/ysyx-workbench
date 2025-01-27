0e39221f97222cb216f0c4a0a5f20d7ffd2ce075
tracer-ysyx <tracer@ysyx.org>
2025-01-27 17:44:21 +0800

    > compile NEMU ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:44:21 up  9:22,  1 user,  load average: 0.97, 0.55, 0.27

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,41 +13,34 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...){
-   //panic("Not implemented");
-   va_list args;
-   va_start(args, fmt);
-   
-   char *ptr = out;
-   const char *p = fmt;
-   
-   while (*p) {
-       if (*p == '%' && (*(p + 1) == 's' || *(p + 1) == 'd')) {
-           p++; // Skip '%'
-           if (*p == 's') {
-               char *str = va_arg(args, char *);
-               while (*str) {
-                   *ptr++ = *str++;
-               }
-           } else if (*p == 'd') {
-               int num = va_arg(args, int);
-               char num_str[12]; // Enough to hold INT_MIN
-               snprintf(num_str, sizeof(num_str), "%d", num);
-               char *num_ptr = num_str;
-               while (*num_ptr) {
-                   *ptr++ = *num_ptr++;
-               }
-           }
-       } else {
-           *ptr++ = *p;
-       }
-       p++;
-   }
-   
-   *ptr = '\0'; // Null-terminate the output string
-   va_end(args);
-   
-   return ptr - out; // Return the number of characters written
+int sprintf(char *out, const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+
+    const char *p = fmt;
+    char *out_ptr = out;
+
+    while (*p) {
+        if (*p == '%') {
+            p++; // Skip the '%'
+            if (*p == 'd') {
+                int num = va_arg(args, int);
+                out_ptr += sprintf(out_ptr, "%d", num);
+            } else if (*p == 's') {
+                const char *str = va_arg(args, const char *);
+                out_ptr += sprintf(out_ptr, "%s", str);
+            }
+            // Add more format specifiers as needed
+        } else {
+            *out_ptr++ = *p;
+        }
+        p++;
+    }
+
+    *out_ptr = '\0'; // Null-terminate the output string
+    va_end(args);
+
+    return (int)(out_ptr - out); // Return the number of characters written
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
