c927bb6f09e1acc2af93c6ba6d07caec1a36c525
tracer-ysyx <tracer@ysyx.org>
2025-01-28 17:49:03 +0800

    > compile NEMU ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:49:03 up 22:43,  1 user,  load average: 0.16, 0.39, 0.43

--- nemu/src/utils/elf_processor.c
+++ nemu/src/utils/elf_processor.c
@@ -85,7 +85,7 @@ void init_elf(const char *elf_file){
     Elf32_Shdr *shdrs = malloc(ehdr.e_shentsize * ehdr.e_shnum);
 
     if(fread(shdrs, ehdr.e_shentsize, ehdr.e_shnum, file) != 1){
-        perror("fread");
+        //perror("fread");
     }
 
     for (int i = 0; i < ehdr.e_shnum; i++) {
