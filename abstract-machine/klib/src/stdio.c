07337c84a9eb3940de78455ccb1ea7c17de7a0ef
tracer-ysyx <tracer@ysyx.org>
2025-01-29 21:01:51 +0800

    > compile NEMU ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:01:51 up 2 days,  1:56,  1 user,  load average: 0.32, 0.44, 0.29

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -71,6 +71,7 @@ int printf(const char *fmt, ...) {
                 }
             }
             if(*(p+1) == 'd'){
+                putch('H');
                 int width = *p - '0';
                 p++;
                 int num = va_arg(args, int);
