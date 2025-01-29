46ba2061e54ae370c628cf396a5f219c1b04fc1d
tracer-ysyx <tracer@ysyx.org>
2025-01-29 15:06:25 +0800

    > compile NEMU ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:06:25 up 1 day, 20:00,  1 user,  load average: 0.70, 0.45, 0.36

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -12,7 +12,7 @@ int printf(const char *fmt, ...) {
     for (const char *p = buf; *p != '\0'; p++) {
         putch(*p);
     }
-    return res;
+    return res+1;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
