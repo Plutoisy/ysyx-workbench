75859e5444114636c9c05caf71215ffd69aa64f2
tracer-ysyx <tracer@ysyx.org>
2025-01-30 23:19:28 +0800

    > compile NEMU ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:19:28 up 3 days,  4:13,  1 user,  load average: 0.01, 0.12, 0.18

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -22,14 +22,14 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  int x = ctl->x;
-  int y = ctl->y;
+  // int x = ctl->x;
+  // int y = ctl->y;
   int w = ctl->w;
   int h = ctl->h;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (int i = 0; i < h; i ++){
     for (int j = 0; i < w; j ++){
-      fb[disp_w*(y+i-1)+x+j-1] = 0x00FFFFFF;
+      fb[disp_w*(0+i-1)+0+j-1] = 0x00FFFFFF;
     }
   }
   if (ctl->sync) {
