e05d29823e315f9f2681df38f71ae63db4b6f035
tracer-ysyx <tracer@ysyx.org>
2025-01-31 20:30:02 +0800

    > compile NEMU ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:30:02 up 3 days, 20:43,  1 user,  load average: 1.39, 0.71, 0.42

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -36,6 +36,7 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
   int len = ctl->buf.end - ctl->buf.start;
   uint8_t *start = ctl->buf.start;
   int count = inl(AUDIO_COUNT_ADDR);
+  printf("%d\n",count);
   for(int i = 0; i < len; i++){
     if (count != 0x10000)
     {
