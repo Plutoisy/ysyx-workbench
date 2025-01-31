64a6d2d57f8d9acf99759ee39b37b8f81ff14060
tracer-ysyx <tracer@ysyx.org>
2025-01-31 20:39:02 +0800

    > compile NEMU ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:39:02 up 3 days, 20:52,  1 user,  load average: 1.00, 0.76, 0.57

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -9,7 +9,7 @@
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
 
-// static int w_idx = 0;
+static int w_idx = 0;
 
 void __am_audio_init() {
   outl(AUDIO_SBUF_SIZE_ADDR, 0x10000);
@@ -32,27 +32,27 @@ void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
   stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
-void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
-  int len = ctl->buf.end - ctl->buf.start;
-  uint8_t *start = ctl->buf.start;
-  for(int i = 0; i < len ; i++){
-    outb(AUDIO_SBUF_ADDR + inl(AUDIO_COUNT_ADDR), *(start + i));
-  }
-}
 // void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
 //   int len = ctl->buf.end - ctl->buf.start;
 //   uint8_t *start = ctl->buf.start;
-//   int count = inl(AUDIO_COUNT_ADDR);
-//   printf("%d\n",w_idx % inl(AUDIO_SBUF_SIZE_ADDR));
-//   for(int i = 0; i < len; i++){
-//     // if (count != 0x10000)
-//     // {
-//       outb(AUDIO_SBUF_ADDR + count, *start);
-//       w_idx++;
-//       start++;
-//       count++;
-//     //}
+//   for(int i = 0; i < len ; i++){
+//     outb(AUDIO_SBUF_ADDR + inl(AUDIO_COUNT_ADDR), *(start + i));
 //   }
-//   outl(AUDIO_COUNT_ADDR, count);
 // }
+void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+  int len = ctl->buf.end - ctl->buf.start;
+  uint8_t *start = ctl->buf.start;
+  int count = inl(AUDIO_COUNT_ADDR);
+  printf("%d\n",w_idx % inl(AUDIO_SBUF_SIZE_ADDR));
+  for(int i = 0; i < len; i++){
+    // if (count != 0x10000)
+    // {
+      outb(AUDIO_SBUF_ADDR + inl(AUDIO_COUNT_ADDR), *start);
+      w_idx++;
+      start++;
+      count++;
+    //}
+  }
+  outl(AUDIO_COUNT_ADDR, count);
+}
 
