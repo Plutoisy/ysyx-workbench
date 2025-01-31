91a2b2b9b1e09b39b2f6dc6454fb800d77ae604a
tracer-ysyx <tracer@ysyx.org>
2025-01-31 19:41:11 +0800

    > compile NEMU ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:41:11 up 3 days, 19:55,  1 user,  load average: 0.62, 0.34, 0.24

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -17,6 +17,8 @@
 #include <device/map.h>
 #include <SDL2/SDL.h>
 
+#define MIN(a,b) (((a)<(b))?(a):(b))
+
 enum {
   reg_freq,
   reg_channels,
@@ -27,19 +29,70 @@ enum {
   nr_reg
 };
 
+#define OFFSET(name) (4 * (name))
+
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
 
-static void audio_io_handler(uint32_t offset, int len, bool is_write) {
-  SDL_AudioSpec s = {};
-  s.format = AUDIO_S16SYS;  // 假设系统中音频数据的格式总是使用16位有符号数来表示
-  s.userdata = NULL;        // 不使用
-  s.freq = audio_base[reg_freq];
-  s.channels = audio_base[reg_channels];
-  s.samples = audio_base[reg_samples];
-  SDL_InitSubSystem(SDL_INIT_AUDIO);
-  SDL_OpenAudio(&s, NULL);
-  SDL_PauseAudio(0);
+static int r_idx = 0;
+
+static void audio_play(void *userdata, uint8_t *stream, int len)
+{
+  int nread = MIN(audio_base[reg_count], len);
+  int sbuf_size = audio_base[reg_sbuf_size];
+  assert(sbuf_size > 0);
+
+  int b = 0;
+  int count = audio_base[reg_count];
+  while (b < nread) {
+    if (count != 0)
+    {
+      *stream = sbuf[r_idx % sbuf_size];    
+      r_idx++;
+      b++;
+      stream++;
+      count--;
+    }
+  }  
+  audio_base[reg_count] -= nread;
+  if (len > nread) {
+    memset(stream, 0, len - nread);
+  }
+}
+
+static void audio_io_handler(uint32_t offset, int len, bool is_write)
+{
+  assert(offset >= 0 && offset <= 20 && offset % 4 == 0);
+  switch (offset)
+  {
+  // case OFFSET(reg_freq):
+  //   break;
+  // case OFFSET(reg_channels):
+  //   break;
+  // case OFFSET(reg_samples):
+  //   break;
+  // case OFFSET(reg_sbuf_size):
+  //   break;
+  case OFFSET(reg_init):
+    if (is_write)
+    {
+      SDL_AudioSpec s = {};
+      s.freq = audio_base[reg_freq];
+      s.format = AUDIO_S16SYS;
+      s.channels = audio_base[reg_channels];
+      s.samples = audio_base[reg_samples];
+      s.callback = audio_play;
+      s.userdata = NULL;
+      if (SDL_InitSubSystem(SDL_INIT_AUDIO) == 0)
+      {
+        SDL_OpenAudio(&s, NULL);
+        SDL_PauseAudio(0);
+      }
+    }
+    break;
+    // case OFFSET(reg_count):
+    //   break;
+  }
 }
 
 void init_audio() {
