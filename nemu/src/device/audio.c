cbc272a73c5c80e86917ed65a737f280deb7d852
tracer-ysyx <tracer@ysyx.org>
2025-01-31 19:45:32 +0800

    > compile NEMU ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:45:32 up 3 days, 19:59,  1 user,  load average: 1.46, 0.76, 0.42

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -1,23 +1,11 @@
-/***************************************************************************************
-* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
-
 #include <common.h>
 #include <device/map.h>
 #include <SDL2/SDL.h>
 
-enum {
+#define MIN(a,b) (((a)<(b))?(a):(b))
+
+enum
+{
   reg_freq,
   reg_channels,
   reg_samples,
@@ -27,27 +15,78 @@ enum {
   nr_reg
 };
 
+#define OFFSET(name) (4 * (name))
+
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
 
-static void audio_io_handler(uint32_t offset, int len, bool is_write) {
-  SDL_AudioSpec s = {};
-  s.format = AUDIO_S16SYS;  // 假设系统中音频数据的格式总是使用16位有符号数来表示
-  s.userdata = NULL;        // 不使用
-  s.freq = audio_base[reg_freq];
-  s.channels = audio_base[reg_freq];
-  s.samples = audio_base[reg_freq];
-  // s.callback = audio_play;
-  SDL_InitSubSystem(SDL_INIT_AUDIO);
-  SDL_OpenAudio(&s, NULL);
-  SDL_PauseAudio(0);
+static int r_idx = 0;
+
+static void audio_play(void *userdata, uint8_t *stream, int len)
+{
+  int nread = MIN(audio_base[reg_count], len);
+  int sbuf_size = audio_base[reg_sbuf_size];
+  assert(sbuf_size > 0);
+
+  int b = 0;
+  int count = audio_base[reg_count];
+  while (b < nread) {
+    if (count != 0)
+    {
+      *stream = sbuf[r_idx % sbuf_size];    
+      r_idx++;
+      b++;
+      stream++;
+      count--;
+    }
+  }  
+  audio_base[reg_count] -= nread;
+  if (len > nread) {
+    memset(stream, 0, len - nread);
+  }
 }
 
-void init_audio() {
+static void audio_io_handler(uint32_t offset, int len, bool is_write)
+{
+  assert(offset >= 0 && offset <= 20 && offset % 4 == 0);
+  switch (offset)
+  {
+  // case OFFSET(reg_freq):
+  //   break;
+  // case OFFSET(reg_channels):
+  //   break;
+  // case OFFSET(reg_samples):
+  //   break;
+  // case OFFSET(reg_sbuf_size):
+  //   break;
+  case OFFSET(reg_init):
+    if (is_write)
+    {
+      SDL_AudioSpec s = {};
+      s.freq = audio_base[reg_freq];
+      s.format = AUDIO_S16SYS;
+      s.channels = audio_base[reg_channels];
+      s.samples = audio_base[reg_samples];
+      s.callback = audio_play;
+      s.userdata = NULL;
+      if (SDL_InitSubSystem(SDL_INIT_AUDIO) == 0)
+      {
+        SDL_OpenAudio(&s, NULL);
+        SDL_PauseAudio(0);
+      }
+    }
+    break;
+    // case OFFSET(reg_count):
+    //   break;
+  }
+}
+
+void init_audio()
+{
   uint32_t space_size = sizeof(uint32_t) * nr_reg;
   audio_base = (uint32_t *)new_space(space_size);
 #ifdef CONFIG_HAS_PORT_IO
-  add_pio_map ("audio", CONFIG_AUDIO_CTL_PORT, audio_base, space_size, audio_io_handler);
+  add_pio_map("audio", CONFIG_AUDIO_CTL_PORT, audio_base, space_size, audio_io_handler);
 #else
   add_mmio_map("audio", CONFIG_AUDIO_CTL_MMIO, audio_base, space_size, audio_io_handler);
 #endif
