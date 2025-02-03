2b2b2388d7a5ee84ada86b8223f428b096503439
tracer-ysyx <tracer@ysyx.org>
2025-02-03 17:10:33 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:10:33 up 4 days, 18:12,  1 user,  load average: 0.62, 0.41, 0.37

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -87,7 +87,7 @@ extern "C" void ebreak(){
 int main() {
   const char *filename = "/home/plutoisy/ysyx-workbench/am-kernels/tests/cpu-tests/build/dummy-riscv32e-npc.bin";
   read_bin_file(filename);
-  // sim_init();
+  sim_init();
   // system_rst();
   // while (trap != 1) {
   //   top->clk ^= 1;
