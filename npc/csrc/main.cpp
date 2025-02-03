594e7c4780258eaef89c5d4f49bd8bf8f82c7de4
tracer-ysyx <tracer@ysyx.org>
2025-02-03 16:54:12 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:54:12 up 4 days, 17:56,  1 user,  load average: 0.59, 0.61, 0.44

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -4,10 +4,44 @@
 #include <cstdint>
 #include <stdio.h>
 
+#define CONFIG_MBASE 0x80000000
+
 VerilatedContext* contextp = NULL;
 VerilatedVcdC* tfp = NULL;
 static Vysyx_24120011_top* top;
 int trap = 0;
+static char *img_file = NULL;
+
+uint32_t pmem[] = {
+    // 0x00230293, //addi t0, t1, 2
+    // 0x00328393, //addi t2, t0, 3
+    // 0x00100073, //ebreak
+};
+
+uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
+
+static long load_img() {
+  if (img_file == NULL) {
+    printf("No image is given. Use the default build-in image.");
+    return 4096; // built-in image size
+  }
+
+  FILE *fp = fopen(img_file, "rb");
+  printf("Can not open '%s'", img_file);
+
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
+
+  printf("The image is %s, size = %ld", img_file, size);
+
+  fseek(fp, 0, SEEK_SET);
+  int ret = fread(guest_to_host(CONFIG_MBASE), size, 1, fp);
+  assert(ret == 1);
+
+  fclose(fp);
+  return size;
+}
+
 void step_and_dump_wave(){
   top->eval();
   contextp->timeInc(1);
@@ -44,12 +78,6 @@ void system_rst(){
   top->rst = 0;
 }
 
-uint32_t memory[] = {
-    0x00230293, //addi t0, t1, 2
-    0x00328393, //addi t2, t0, 3
-    0x00100073, //ebreak
-};
-
 uint32_t pmem_read(uint32_t pc) {
     uint32_t index = (pc - 0x80000000) / 4; 
     return memory[index];
@@ -62,6 +90,8 @@ extern "C" void ebreak(){
 }
 
 int main() {
+  img_file = "/home/plutoisy/ysyx-workbench/am-kernels/tests/cpu-tests/build/dummy-riscv32e-npc.bin"
+  load_img();
   sim_init();
   system_rst();
   while (trap != 1) {
