bfded5665c1b9304134e3eaccbfbd59f667fbdfe
tracer-ysyx <tracer@ysyx.org>
2025-02-03 17:26:26 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:26:26 up 4 days, 18:28,  1 user,  load average: 0.58, 0.60, 0.56

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -84,7 +84,7 @@ void system_rst(){
 }
 
 uint32_t pmem_read(uint32_t pc) {
-    if((pc - 0x80000000) / 4 > 0x80000000){
+    if((pc - 0x80000000) / 4 > 12){
       printf("Out Of Mem!!!");
       assert(0);
     }
