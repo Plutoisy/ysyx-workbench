a043de9dc35e878a2d7db9d79d7c1e604ddbb849
tracer-ysyx <tracer@ysyx.org>
2025-02-03 16:41:27 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:41:27 up 4 days, 17:43,  1 user,  load average: 0.28, 0.27, 0.21

--- npc/vsrc/ysyx_24120011_ALUCtrl.v
+++ npc/vsrc/ysyx_24120011_ALUCtrl.v
@@ -2,7 +2,7 @@ module ysyx_24120011_ALUCtrl(
     input ALUBctrl,
     input [31:0] src2,
     input [31:0] imme,
-    output ALUB
+    output [31:0] ALUB
 );
 //ALUBctrl == 1'd0 -> imme
 //ALUBctrl == 1'd1 -> src2
