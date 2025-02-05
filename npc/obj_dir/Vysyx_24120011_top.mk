cceabf590c35b86c5624506d66e1177559701084
tracer-ysyx <tracer@ysyx.org>
2025-02-05 20:46:31 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:46:31 up  1:51,  1 user,  load average: 0.07, 0.11, 0.11

--- npc/obj_dir/Vysyx_24120011_top.mk
+++ npc/obj_dir/Vysyx_24120011_top.mk
@@ -4,7 +4,7 @@
 # Execute this makefile from the object directory:
 #    make -f Vysyx_24120011_top.mk
 
-default: Vysyx_24120011_top
+default: Vysyx_24120011_top__ALL.a
 
 ### Constants...
 # Perl executable (from $PERL)
@@ -35,19 +35,17 @@ VM_PREFIX = Vysyx_24120011_top
 VM_MODPREFIX = Vysyx_24120011_top
 # User CFLAGS (from -CFLAGS on Verilator command line)
 VM_USER_CFLAGS = \
+	-I /home/plutoisy/ysyx-workbench/npc/tools/capstone/repo/include \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
-	-L/home/plutoisy/ysyx-workbench/nemu/build -L/home/plutoisy/ysyx-workbench/npc/tools/capstone/repo -lriscv32-nemu-interpreter -lcapstone -lreadline -Wl,-rpath,/home/plutoisy/ysyx-workbench/nemu/build \
 
 # User .cpp files (from .cpp's on Verilator command line)
 VM_USER_CLASSES = \
 	main \
-	main \
 
 # User .cpp directories (from .cpp's on Verilator command line)
 VM_USER_DIR = \
-	. \
 	/home/plutoisy/ysyx-workbench/npc/csrc \
 
 
@@ -56,18 +54,7 @@ VM_USER_DIR = \
 include Vysyx_24120011_top_classes.mk
 # Include global rules
 include $(VERILATOR_ROOT)/include/verilated.mk
-
-### Executable rules... (from --exe)
-VPATH += $(VM_USER_DIR)
-
 main.o: /home/plutoisy/ysyx-workbench/npc/csrc/main.cpp
 	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
-main.o: main.cpp
-	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
-
-### Link rules... (from --exe)
-Vysyx_24120011_top: $(VK_USER_OBJS) $(VK_GLOBAL_OBJS) $(VM_PREFIX)__ALL.a $(VM_HIER_LIBS)
-	$(LINK) $(LDFLAGS) $^ $(LOADLIBES) $(LDLIBS) $(LIBS) $(SC_LIBS) -o $@
-
 
 # Verilated -*- Makefile -*-
