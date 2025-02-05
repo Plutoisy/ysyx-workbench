121b3b5f0f4b2df001f5379859e1f87327804863
tracer-ysyx <tracer@ysyx.org>
2025-02-05 18:53:22 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:53:22 up 6 days, 16:03,  1 user,  load average: 0.60, 0.71, 0.57

--- npc/obj_dir/Vysyx_24120011_top.mk
+++ npc/obj_dir/Vysyx_24120011_top.mk
@@ -38,7 +38,7 @@ VM_USER_CFLAGS = \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
-	-L/home/plutoisy/ysyx-workbench/nemu/build -lriscv32-nemu-interpreter -lreadline \
+	-L/home/plutoisy/ysyx-workbench/nemu/build -lriscv32-nemu-interpreter -lreadline -Wl -rpath /home/plutoisy/ysyx-workbench/nemu/build \
 
 # User .cpp files (from .cpp's on Verilator command line)
 VM_USER_CLASSES = \
