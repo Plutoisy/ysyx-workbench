21f08d3bdecfe0aceb8623d9216d681e0c429067
tracer-ysyx <tracer@ysyx.org>
2025-02-06 11:03:46 +0800

    > compile NEMU ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:03:46 up  4:59,  1 user,  load average: 0.24, 0.21, 0.20

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -30,7 +30,14 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
 }
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
+  if (direction == DIFFTEST_TO_REF) {
+    cpu.pc = 0x80000000;
+    for(int i = 0; i < 32; i++){
+      cpu.gpr[i] = 0x00000000;
+    }
+  } else {
+    assert(0);
+  }
 }
 
 __EXPORT void difftest_exec(uint64_t n) {
