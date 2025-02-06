75193810b71f939b3333138731cfab428092e689
tracer-ysyx <tracer@ysyx.org>
2025-02-06 15:28:54 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:28:54 up  9:24,  1 user,  load average: 0.93, 0.79, 0.50

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -234,18 +234,22 @@ void cpu_exec(uint32_t n){
       AssembleDecoder(handle, top->inst, top->pc);
       difftest_exec(1);
       difftest_regcpy(&refstate, 0);
-      step_and_dump_wave();
-      for(int j = 0; j < 32; j++){
-        printf("0x%08x\n",top->pc);
-        printf("0x%08x\n",refstate.pc);
-        if(gpr[j] == refstate.gpr[j]){
-          printf("PASS\n");
-        }
-        else{
-          assert(0);
-        }
+      if(refstate.pc == top->pc){
+        printf("PASS\n");
       }
-      
+      else{
+        assert(0);
+      }
+      for(int j = 0; j < 32; j++){
+        printf("0x%08x\n",refstate.gpr[j]);
+        // if(gpr[j] == refstate.gpr[j]){
+        //   printf("PASS\n");
+        // }
+        // else{
+        //   assert(0);
+        // }
+      }
+      step_and_dump_wave();
     }
     else{
       printf("\33[1;34mProgram execution has ended. To restart the program, exit npc and run again.\033[0m\n");
