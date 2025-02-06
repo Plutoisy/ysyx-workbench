70f378fd63762760a40266733ca6471db9be322d
tracer-ysyx <tracer@ysyx.org>
2025-02-06 11:05:58 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:05:58 up  5:01,  1 user,  load average: 0.64, 0.33, 0.23

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -370,7 +370,7 @@ int main(int argc, char *argv[]) {
   load_img();
   difftest_memcpy(CONFIG_MBASE, pmem, PMEM_SIZE, 1);
   void* dut;
-  difftest_regcpy(dut, 1)
+  difftest_regcpy(dut, 1);
   sim_init();
   system_rst();
   sdb_mainloop();
