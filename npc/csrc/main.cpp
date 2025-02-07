4815bb3888f498bf7a53f405fb8ca7a0a23c4a62
tracer-ysyx <tracer@ysyx.org>
2025-02-07 15:08:32 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:08:32 up 22:35,  1 user,  load average: 1.30, 0.81, 0.39

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -174,7 +174,7 @@ static inline void host_write(void *addr, int len, uint32_t data) {
 }
 
 static uint32_t pmem_read(uint32_t addr, int len) {
-  if(paddr - CONFIG_MBASE > PMEM_SIZE){
+  if(addr - CONFIG_MBASE > PMEM_SIZE){
     assert(0);
   }
   uint32_t ret = host_read(guest_to_host(addr), len);
