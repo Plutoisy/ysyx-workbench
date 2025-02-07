096722c7bf14de7fdd01a024dcc2854e5ec6483e
tracer-ysyx <tracer@ysyx.org>
2025-02-07 14:21:43 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:21:43 up 21:48,  1 user,  load average: 0.15, 0.15, 0.18

--- npc/vsrc/ysyx_24120011_MemProcessor.v
+++ npc/vsrc/ysyx_24120011_MemProcessor.v
@@ -13,16 +13,13 @@ module ysyx_24120011_MemProcessor(
 );
 reg [31:0] r_mem_data_tmp;
 always@(*)begin
-    if(w_mem_en)begin
+    if(w_mem_en && !r_mem_en)begin
         rtl_pmem_write(w_mem_addr,w_mem_data,w_mem_len);
         r_mem_data_tmp = 32'b00000000;
         r_mem_data = 32'b00000000;
     end
-    else begin
-        r_mem_data_tmp = 32'b00000000;
-        r_mem_data = 32'b00000000;
-    end
-    if(r_mem_en)begin
+    
+    else if(r_mem_en && !w_mem_en)begin
         r_mem_data_tmp = rtl_pmem_read(r_mem_addr);
         if(r_mem_len == 8'd1)begin
             if(sign_extension)begin
@@ -32,7 +29,7 @@ always@(*)begin
                 r_mem_data = {24'b0,r_mem_data_tmp[7:0]};
             end
         end
-        if(r_mem_len == 8'd2)begin
+        else if(r_mem_len == 8'd2)begin
             if(sign_extension)begin
                 r_mem_data = {{16{r_mem_data_tmp[7]}},r_mem_data_tmp[15:0]};
             end
@@ -40,13 +37,18 @@ always@(*)begin
                 r_mem_data = {16'b0,r_mem_data_tmp[15:0]};
             end
         end
-        if(r_mem_len == 8'd4)begin
+        else if(r_mem_len == 8'd4)begin
             r_mem_data = r_mem_data_tmp;
         end
+
+        else begin//shouldn't in
+            r_mem_data = 32'b11111111;
+        end
     end
-    else begin
-        r_mem_data_tmp = 32'b00000000;
-        r_mem_data = 32'b00000000;
+
+    else begin//shouldn't in
+        r_mem_data_tmp = 32'b11111111;
+        r_mem_data = 32'b11111111;
     end
 end
 endmodule
