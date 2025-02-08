8c8efc44c1f4727acffcbf25f51632275a831ea2
tracer-ysyx <tracer@ysyx.org>
2025-02-08 12:55:18 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:55:18 up 1 day,  9:19,  1 user,  load average: 1.24, 0.56, 0.31

--- npc/vsrc/ysyx_24120011_ImmeGen.v
+++ npc/vsrc/ysyx_24120011_ImmeGen.v
@@ -9,6 +9,7 @@ module ysyx_24120011_ImmeGen(
 // 3'd2; J-Type
 // 3'd3; S-Type
 // 3'd4; R-Type
+// 3'd5; B-Type
 // 3'd7; Unknown
 
 always@(*)begin
@@ -17,6 +18,7 @@ always@(*)begin
         3'd1      : imme = {inst[31:12],12'b0};//U-Type
         3'd2      : imme = {{11{inst[31]}},inst[31],inst[19:12],inst[20],inst[30:21],1'b0};//J-Type
         3'd3      : imme = {{20{inst[31]}},inst[31:25],inst[11:7]};//S-Type
+        3'd5      : imme = {{20{inst[31]}},inst[7],inst[30:25],inst[11:8],1'b0};//B-Type
         default   : imme = 32'h0000_0000;//Unknown/R-Type
     endcase
 end
--- npc/vsrc/ysyx_24120011_TypeFinder.v
+++ npc/vsrc/ysyx_24120011_TypeFinder.v
@@ -16,6 +16,8 @@ module ysyx_24120011_TypeFinder(
             7'b0100011:opcode_type = 3'd3;//S-Type
 
             7'b0110011:opcode_type = 3'd4;//R-Type
+
+            7'b1100011:opcode_type = 3'd5;//B-Type
             default   :opcode_type = 3'd7;//Unknown
         endcase
     end
