f68df16ea9c210ff3a3dff1b154295763dbbc29c
tracer-ysyx <tracer@ysyx.org>
2025-02-08 21:28:36 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:28:36 up 1 day, 17:52,  1 user,  load average: 0.00, 0.00, 0.08

--- npc/obj_dir/Vysyx_24120011_top___024root__DepSet_h33b7d665__0__Slow.cpp
+++ npc/obj_dir/Vysyx_24120011_top___024root__DepSet_h33b7d665__0__Slow.cpp
@@ -242,7 +242,14 @@ VL_ATTR_COLD void Vysyx_24120011_top___024root___stl_sequent__TOP__0(Vysyx_24120
                                                     & (vlSelf->inst 
                                                        >> 0xcU)))
                                                    ? 3U
-                                                   : 0U)))));
+                                                   : 
+                                                  ((6U 
+                                                    == 
+                                                    (7U 
+                                                     & (vlSelf->inst 
+                                                        >> 0xcU)))
+                                                    ? 0xbU
+                                                    : 0U))))));
             vlSelf->ysyx_24120011_top__DOT__imme = 
                 (((- (IData)((vlSelf->inst >> 0x1fU))) 
                   << 0xcU) | ((0x800U & (vlSelf->inst 
