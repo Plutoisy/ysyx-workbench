62d84e211778ffda4383240c7b733096ed043662
tracer-ysyx <tracer@ysyx.org>
2025-02-09 15:50:34 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:50:34 up 1 day, 22:43,  1 user,  load average: 0.47, 0.24, 0.15

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -228,7 +228,7 @@ extern "C" void rtl_pmem_write (int w_mem_addr, int w_mem_data, char w_mem_len){
   if(w_mem_addr - CONFIG_MBASE > PMEM_SIZE){
     assert(0);
   }
-  printf("W->addr: 0x%x, len: %d, mem: 0x%08x\n", w_mem_addr, w_mem_len, w_mem_data);
+  //printf("W->addr: 0x%x, len: %d, mem: 0x%08x\n", w_mem_addr, w_mem_len, w_mem_data);
   host_write(guest_to_host(w_mem_addr), w_mem_len, w_mem_data);
 }
 
@@ -237,7 +237,7 @@ extern "C" int rtl_pmem_read(int r_mem_addr){
     assert(0);
   }
   uint32_t ret = host_read(guest_to_host(r_mem_addr), 4);
-  printf("R->addr: 0x%x, len: %d, mem: 0x%08x\n", r_mem_addr, 4, ret);
+  //printf("R->addr: 0x%x, len: %d, mem: 0x%08x\n", r_mem_addr, 4, ret);
   return ret;
 }
 
