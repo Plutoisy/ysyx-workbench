603dbeace40c8dc52faca74e3d8f7c0c257752a7
tracer-ysyx <tracer@ysyx.org>
2025-02-09 15:50:12 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:50:12 up 1 day, 22:42,  1 user,  load average: 0.27, 0.17, 0.12

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -256,27 +256,29 @@ void cpu_exec(uint32_t n){
       }
       top->inst = pmem_read(top->pc,4);
       // printf("\33[1;34mnpc execute pc = 0x%08x, inst = 0x%08x\033[0m\n",top->pc, top->inst);
-      AssembleDecoder(handle, top->inst, top->pc);
-      printf("exec times: %d\n",i+1);
-      difftest_exec(1);
-      difftest_regcpy(&refstate, 0);
+
+
+      // AssembleDecoder(handle, top->inst, top->pc);
+      // printf("exec times: %d\n",i+1);
+      // difftest_exec(1);
+      // difftest_regcpy(&refstate, 0);
       step_and_dump_wave();
-      printf("        dut                    | ref                   \n");
-      printf("pc      0x%08x             | 0x%08x\n", top->pc, refstate.pc);
-      if(refstate.pc != top->pc){
-        //printf("        0x%08x             | 0x%08x\n", top->pc, refstate.pc);
-        assert(0);
-        //printf("0x%08x\n",refstate.pc );
-        //printf("0x%08x\n",top->pc);
-      }
+      // printf("        dut                    | ref                   \n");
+      // printf("pc      0x%08x             | 0x%08x\n", top->pc, refstate.pc);
+      // if(refstate.pc != top->pc){
+      //   //printf("        0x%08x             | 0x%08x\n", top->pc, refstate.pc);
+      //   assert(0);
+      //   //printf("0x%08x\n",refstate.pc );
+      //   //printf("0x%08x\n",top->pc);
+      // }
       
-      for(int j = 0; j < 32; j++){
-        printf("%-3s     %-10u  0x%08x | %-10u  0x%08x\n", regs[j], gpr[j], gpr[j], refstate.gpr[j], refstate.gpr[j]);
-        if(refstate.gpr[j] != gpr[j]){
-          //printf("%-3s     %-10u  0x%08x | %-10u  0x%08x\n", regs[j], gpr[j], gpr[j], refstate.gpr[j], refstate.gpr[j]);
-          assert(0);
-        }
-      }
+      // for(int j = 0; j < 32; j++){
+      //   printf("%-3s     %-10u  0x%08x | %-10u  0x%08x\n", regs[j], gpr[j], gpr[j], refstate.gpr[j], refstate.gpr[j]);
+      //   if(refstate.gpr[j] != gpr[j]){
+      //     //printf("%-3s     %-10u  0x%08x | %-10u  0x%08x\n", regs[j], gpr[j], gpr[j], refstate.gpr[j], refstate.gpr[j]);
+      //     assert(0);
+      //   }
+      // }
       
     }
     else{
