8769aebdc98ddd5fec21bda0ee1aa979c569c759
tracer-ysyx <tracer@ysyx.org>
2025-02-09 16:46:17 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:46:17 up 1 day, 23:38,  1 user,  load average: 0.50, 0.76, 0.60

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -259,8 +259,9 @@ extern "C" int rtl_pmem_read(int r_mem_addr){
   //printf("R->addr: 0x%x, len: %d, mem: 0x%08x\n", r_mem_addr, 4, ret);
   if(r_mem_addr - CONFIG_MBASE > PMEM_SIZE){
     if (r_mem_addr == 0xa0000048 + 4) { 
-      printf("debug\n");
+      
       uint64_t us = get_time();
+      printf("debug\n");
       rtc_port_base[0] = (uint32_t)us;
       rtc_port_base[1] = us >> 32;
       return rtc_port_base[1];
