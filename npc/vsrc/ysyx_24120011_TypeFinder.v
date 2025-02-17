b24a4a3134635c926143e3b3ca7ccbdc5323036e
tracer-ysyx <tracer@ysyx.org>
2025-02-17 11:59:23 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:59:23 up  5:27,  1 user,  load average: 0.92, 0.31, 0.16

--- npc/vsrc/ysyx_24120011_TypeFinder.v
+++ npc/vsrc/ysyx_24120011_TypeFinder.v
@@ -7,6 +7,7 @@ module ysyx_24120011_TypeFinder(
             7'b0010011:opcode_type = 3'd0;//I-Type
             7'b1100111:opcode_type = 3'd0;//I-Type
             7'b0000011:opcode_type = 3'd0;//I-Type
+            7'b1110011:opcode_type = 3'd0;//I-Type
             
             7'b0010111:opcode_type = 3'd1;//U-Type
             7'b0110111:opcode_type = 3'd1;//U-Type
