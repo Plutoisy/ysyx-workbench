1f975b5a62b9cf277d7d6c16a2cad04e22785aba
tracer-ysyx <tracer@ysyx.org>
2025-02-23 23:05:20 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:05:20 up 12:43,  1 user,  load average: 0.40, 0.19, 0.07

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -301,7 +301,7 @@ void cpu_exec(uint32_t n){
 
 
       // AssembleDecoder(handle, top->inst, top->pc);
-      // printf("exec times: %d\n",i+1);
+      printf("exec times: %d\n",i+1);
       // difftest_exec(1);
       // difftest_regcpy(&refstate, 0);
       step_and_dump_wave();
