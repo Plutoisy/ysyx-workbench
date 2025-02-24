014cf99f48b1e7d43d2720fdec0302b3ecb947c4
tracer-ysyx <tracer@ysyx.org>
2025-02-24 22:15:58 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:15:58 up 22:45,  1 user,  load average: 0.30, 0.32, 0.29

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -313,31 +313,36 @@ void cpu_exec(uint32_t n){
       //   step_and_dump_wave();
       //   top->clk ^= 1;
       // }
-      printf("top_IFU_valid_int: %d\n",top_IFU_valid_int);
-      AssembleDecoder(handle, top_inst, top_pc);
-      //printf("exec times: %d\n",i+1);
-      //difftest_exec(1);
-      //difftest_regcpy(&refstate, 0);
-      step_and_dump_wave();
+      if(top_IFU_valid_int){
+        AssembleDecoder(handle, top_inst, top_pc);
+        //printf("exec times: %d\n",i+1);
+        //difftest_exec(1);
+        //difftest_regcpy(&refstate, 0);
+        step_and_dump_wave();
 
-      //printf("        dut                    | ref                   \n");
-      //printf("pc      0x%08x             | 0x%08x\n", top_pc, refstate.pc);
-      //if(refstate.pc != top_pc){
-      //  //AssembleDecoder(handle, top_inst, top_pc);
-      //  assert(0);
-      //  //printf("0x%08x\n",refstate.pc );
-      //  //printf("0x%08x\n",top_pc);
-      //}
+        //printf("        dut                    | ref                   \n");
+        //printf("pc      0x%08x             | 0x%08x\n", top_pc, refstate.pc);
+        //if(refstate.pc != top_pc){
+        //  //AssembleDecoder(handle, top_inst, top_pc);
+        //  assert(0);
+        //  //printf("0x%08x\n",refstate.pc );
+        //  //printf("0x%08x\n",top_pc);
+        //}
+        
+        // for(int j = 0; j < 32; j++){
+        //   printf("%-3s     %-10u  0x%08x | %-10u  0x%08x\n", regs[j], gpr[j], gpr[j], refstate.gpr[j], refstate.gpr[j]);
+        //   if(refstate.gpr[j] != gpr[j]){
+        //     //AssembleDecoder(handle, top_inst, top_pc);
+        //     //printf("exec times: %d\n",i+1);
+        //     //printf("%-3s     %-10u  0x%08x | %-10u  0x%08x\n", regs[j], gpr[j], gpr[j], refstate.gpr[j], refstate.gpr[j]);
+        //     assert(0);
+        //   }
+        // }
+      }
+      else{
+        step_and_dump_wave();
+      }
       
-      // for(int j = 0; j < 32; j++){
-      //   printf("%-3s     %-10u  0x%08x | %-10u  0x%08x\n", regs[j], gpr[j], gpr[j], refstate.gpr[j], refstate.gpr[j]);
-      //   if(refstate.gpr[j] != gpr[j]){
-      //     //AssembleDecoder(handle, top_inst, top_pc);
-      //     //printf("exec times: %d\n",i+1);
-      //     //printf("%-3s     %-10u  0x%08x | %-10u  0x%08x\n", regs[j], gpr[j], gpr[j], refstate.gpr[j], refstate.gpr[j]);
-      //     assert(0);
-      //   }
-      // }
       
     }
     else{
