4664750b4feffbed20d8758cb669f60842964264
tracer-ysyx <tracer@ysyx.org>
2025-03-01 18:51:28 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:51:28 up 2 days,  2:59,  1 user,  load average: 0.83, 0.39, 0.31

---
+++ npc/obj_dir/Vysyx_24120011_top___024root.h
@@ -0,0 +1,112 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design internal header
+// See Vysyx_24120011_top.h for the primary calling header
+
+#ifndef VERILATED_VYSYX_24120011_TOP___024ROOT_H_
+#define VERILATED_VYSYX_24120011_TOP___024ROOT_H_  // guard
+
+#include "verilated.h"
+
+class Vysyx_24120011_top__Syms;
+class Vysyx_24120011_top___024unit;
+
+
+class Vysyx_24120011_top___024root final : public VerilatedModule {
+  public:
+    // CELLS
+    Vysyx_24120011_top___024unit* __PVT____024unit;
+
+    // DESIGN SPECIFIC STATE
+    // Anonymous structures to workaround compiler member-count bugs
+    struct {
+        VL_IN8(clk,0,0);
+        VL_IN8(rst,0,0);
+        CData/*1:0*/ ysyx_24120011_top__DOT__pc_ctrl;
+        CData/*3:0*/ ysyx_24120011_top__DOT__rd_ctrl;
+        CData/*1:0*/ ysyx_24120011_top__DOT__ALUBctrl;
+        CData/*0:0*/ ysyx_24120011_top__DOT__w_mem_en;
+        CData/*0:0*/ ysyx_24120011_top__DOT__r_mem_en;
+        CData/*0:0*/ ysyx_24120011_top__DOT__sign_extension;
+        CData/*7:0*/ ysyx_24120011_top__DOT__w_mem_len;
+        CData/*7:0*/ ysyx_24120011_top__DOT__r_mem_len;
+        CData/*3:0*/ ysyx_24120011_top__DOT__ALU_ctrl;
+        CData/*3:0*/ ysyx_24120011_top__DOT__w_csr_data_ctrl;
+        CData/*0:0*/ ysyx_24120011_top__DOT__w_csr_en;
+        CData/*0:0*/ ysyx_24120011_top__DOT__r_csr_en;
+        CData/*0:0*/ ysyx_24120011_top__DOT__w_csr_ecall;
+        CData/*0:0*/ ysyx_24120011_top__DOT__LSU_valid;
+        CData/*2:0*/ ysyx_24120011_top__DOT__i_IFU__DOT__u_SRAM__DOT__state;
+        CData/*2:0*/ ysyx_24120011_top__DOT__i_IFU__DOT__u_SRAM__DOT__next_state;
+        CData/*2:0*/ ysyx_24120011_top__DOT__u_ysyx_24120011_IDU__DOT__opcode_type;
+        CData/*0:0*/ ysyx_24120011_top__DOT__u_ysyx_24120011_IDU__DOT____VdfgExtracted_hd7d5a5db__0;
+        CData/*0:0*/ ysyx_24120011_top__DOT__u_ysyx_24120011_IDU__DOT____VdfgExtracted_hecfb244b__0;
+        CData/*0:0*/ ysyx_24120011_top__DOT__u_ysyx_24120011_IDU__DOT____VdfgExtracted_hed3628ff__0;
+        CData/*0:0*/ ysyx_24120011_top__DOT__u_ysyx_24120011_IDU__DOT____VdfgTmp_h53d597ad__0;
+        CData/*0:0*/ ysyx_24120011_top__DOT__i_ALU__DOT__carry;
+        CData/*0:0*/ ysyx_24120011_top__DOT__i_ALU__DOT__sless;
+        CData/*2:0*/ ysyx_24120011_top__DOT__i_LSU__DOT__state;
+        CData/*2:0*/ ysyx_24120011_top__DOT__i_LSU__DOT__next_state;
+        CData/*0:0*/ ysyx_24120011_top__DOT__i_LSU__DOT__start_read_delay;
+        CData/*0:0*/ ysyx_24120011_top__DOT__i_LSU__DOT__start_write_delay;
+        CData/*0:0*/ ysyx_24120011_top__DOT__i_LSU__DOT__LSU_working;
+        CData/*2:0*/ ysyx_24120011_top__DOT__i_LSU__DOT__u_SRAM__DOT__state;
+        CData/*2:0*/ ysyx_24120011_top__DOT__i_LSU__DOT__u_SRAM__DOT__next_state;
+        CData/*5:0*/ __Vtableidx1;
+        CData/*3:0*/ __Vtableidx3;
+        CData/*6:0*/ __Vtableidx4;
+        CData/*0:0*/ __Vtrigrprev__TOP__clk;
+        CData/*0:0*/ __VactContinue;
+        SData/*11:0*/ ysyx_24120011_top__DOT__w_csr_addr;
+        SData/*11:0*/ ysyx_24120011_top__DOT__r_csr_addr;
+        SData/*9:0*/ __Vtableidx2;
+        IData/*31:0*/ ysyx_24120011_top__DOT__dnpc;
+        IData/*31:0*/ ysyx_24120011_top__DOT__imme;
+        IData/*31:0*/ ysyx_24120011_top__DOT__rdata;
+        IData/*31:0*/ ysyx_24120011_top__DOT__r_mem_data;
+        IData/*31:0*/ ysyx_24120011_top__DOT__src1;
+        IData/*31:0*/ ysyx_24120011_top__DOT__src2;
+        IData/*31:0*/ ysyx_24120011_top__DOT__pc_add_imme_out;
+        IData/*31:0*/ ysyx_24120011_top__DOT__ALUB;
+        IData/*31:0*/ ysyx_24120011_top__DOT__alu_result;
+        IData/*31:0*/ ysyx_24120011_top__DOT__w_csr_data;
+        IData/*31:0*/ ysyx_24120011_top__DOT__r_csr_data;
+        IData/*31:0*/ ysyx_24120011_top__DOT__pc;
+        IData/*31:0*/ ysyx_24120011_top__DOT__inst;
+        IData/*31:0*/ ysyx_24120011_top__DOT__i_IFU__DOT__u_SRAM__DOT__addr;
+        IData/*31:0*/ ysyx_24120011_top__DOT__i_ALU__DOT__B_in;
+        IData/*31:0*/ ysyx_24120011_top__DOT__i_ALU__DOT__ALUout_tmp;
+        IData/*31:0*/ ysyx_24120011_top__DOT__i_ALU__DOT____VdfgTmp_h6c4bfc22__0;
+        IData/*31:0*/ ysyx_24120011_top__DOT__i_LSU__DOT__rdata;
+        IData/*31:0*/ ysyx_24120011_top__DOT__i_LSU__DOT__u_SRAM__DOT__addr;
+        IData/*31:0*/ ysyx_24120011_top__DOT__i_Csr__DOT__mepc;
+        IData/*31:0*/ ysyx_24120011_top__DOT__i_Csr__DOT__mstatus;
+        IData/*31:0*/ ysyx_24120011_top__DOT__i_Csr__DOT__mcause;
+        IData/*31:0*/ ysyx_24120011_top__DOT__i_Csr__DOT__mtvec;
+        IData/*31:0*/ __Vfunc_rtl_pmem_read__3__Vfuncout;
+    };
+    struct {
+        IData/*31:0*/ __Vfunc_rtl_pmem_read__6__Vfuncout;
+        IData/*31:0*/ __VstlIterCount;
+        IData/*31:0*/ __VactIterCount;
+        VlUnpacked<IData/*31:0*/, 32> ysyx_24120011_top__DOT__i_RegStack__DOT__Regs;
+        VlUnpacked<IData/*31:0*/, 32> ysyx_24120011_top__DOT__i_RegStack__DOT__regout;
+        VlUnpacked<CData/*0:0*/, 2> __Vm_traceActivity;
+    };
+    VlTriggerVec<1> __VstlTriggered;
+    VlTriggerVec<2> __VactTriggered;
+    VlTriggerVec<2> __VnbaTriggered;
+
+    // INTERNAL VARIABLES
+    Vysyx_24120011_top__Syms* const vlSymsp;
+
+    // CONSTRUCTORS
+    Vysyx_24120011_top___024root(Vysyx_24120011_top__Syms* symsp, const char* v__name);
+    ~Vysyx_24120011_top___024root();
+    VL_UNCOPYABLE(Vysyx_24120011_top___024root);
+
+    // INTERNAL METHODS
+    void __Vconfigure(bool first);
+} VL_ATTR_ALIGNED(VL_CACHE_LINE_BYTES);
+
+
+#endif  // guard
