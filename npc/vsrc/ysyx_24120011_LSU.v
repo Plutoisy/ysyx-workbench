3f7cb932479c7e87b083e8fc2a01a938b59fdebc
tracer-ysyx <tracer@ysyx.org>
2025-03-01 19:36:20 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:36:20 up 2 days,  3:43,  1 user,  load average: 0.27, 0.27, 0.22

--- npc/vsrc/ysyx_24120011_LSU.v
+++ npc/vsrc/ysyx_24120011_LSU.v
@@ -97,14 +97,12 @@ module ysyx_24120011_LSU(
             if(LSU_working == 0 && (w_mem_en == 0 && r_mem_en == 0) )begin
                 LSU_valid <= 1;
             end
-        end
-        else begin
-            if(LSU_working == 1 && next_state == ysyx_24120011_M_AXI_IDLE)begin
+            else if(LSU_working == 1 && next_state == ysyx_24120011_M_AXI_IDLE)begin
                 LSU_valid <= 1;
             end
-            else begin
-                LSU_valid <= 0;
-            end
+        end
+        else begin
+            LSU_valid <= 0;
         end
     end
 
