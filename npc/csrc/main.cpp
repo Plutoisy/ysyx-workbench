3ae302448a4e8f7f07b821438f2c8c2d0655d3ab
tracer-ysyx <tracer@ysyx.org>
2025-03-01 17:22:37 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:22:37 up 2 days,  1:30,  1 user,  load average: 0.31, 0.34, 0.34

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -310,9 +310,9 @@ void cpu_exec(uint32_t n){
         top->clk ^= 1;
       }
       printf("top_IFU_valid_int:%d\n",top_IFU_valid_int);
-      AssembleDecoder(handle, top_inst, top_pc);
+      //AssembleDecoder(handle, top_inst, top_pc);
       if(top_IFU_valid_int){
-        //AssembleDecoder(handle, top_inst, top_pc);
+        AssembleDecoder(handle, top_inst, top_pc);
         printf("exec times: %d\n",i+1);
         difftest_exec(1);
         difftest_regcpy(&refstate, 0);
