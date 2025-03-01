e37cc3822c9ca8974a9a50dd3a2ecbc792c0fbdd
tracer-ysyx <tracer@ysyx.org>
2025-03-01 16:55:03 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:55:03 up 2 days,  1:02,  1 user,  load average: 0.15, 0.25, 0.26

--- npc/vsrc/ysyx_24120011_LSU.v
+++ npc/vsrc/ysyx_24120011_LSU.v
@@ -16,75 +16,203 @@ module ysyx_24120011_LSU(
     output LSU_valid
 );
 
-reg [31:0] r_mem_data_tmp;
-reg LSU_valid_tmp;
+    SRAM u_SRAM(
+        .clk     ( clk     ),
+        .rst     ( rst     ),
+        .araddr  ( araddr  ),
+        .arvalid ( arvalid ),
+        .arready ( arready ),
+        .rdata   ( rdata   ),
+        .rresp   ( rresp   ),
+        .rvalid  ( rvalid  ),
+        .rready  ( rready  ),
+        .awaddr  ( awaddr  ),
+        .awvalid ( awvalid ),
+        .awready ( awready ),
+        .wdata   ( wdata   ),
+        .wstrb   ( wstrb   ),
+        .wvalid  ( wvalid  ),
+        .wready  ( wready  ),
+        .bresp   ( bresp   ),
+        .bvalid  ( bvalid  ),
+        .bready  ( bready  )
+    );
 
-SRAMold i_SRAMold(
-    .clk              ( clk              ),
-    .rst              ( rst              ),
-    .sram_rd_en       ( r_mem_en         ),      
-    .sram_rd_addr     ( r_mem_addr       ),
-    .sram_rd_data_out ( r_mem_data_tmp   ),
-    .sram_valid       ( LSU_valid_tmp    )
-);
+    parameter ysyx_24120011_M_AXI_IDLE  = 3'b000;
+    parameter ysyx_24120011_M_AXI_RADDR = 3'b001;
+    parameter ysyx_24120011_M_AXI_RDATA = 3'b010;
+    parameter ysyx_24120011_M_AXI_WADDR = 3'b011;
+    parameter ysyx_24120011_M_AXI_WDATA = 3'b100;
+    parameter ysyx_24120011_M_AXI_WRESP = 3'b101;
+    parameter ysyx_24120011_M_AXI_RESP_OKAY = 2'b00;
 
+    reg [2:0] state;
+    reg [2:0] next_state;
+    reg start_read_delay;
+    reg start_write_delay;
 
+    wire [31:0] araddr;
+    wire arvalid;
+    wire rready;
+    wire awvalid;
+    wire [31:0] awaddr;
+    wire wvalid;
+    wire [31:0] wdata;
+    wire [3:0] wstrb;
+    wire bready;
+    wire [31:0] rdata;
 
-always@(posedge clk)begin
-    if(w_mem_en && !r_mem_en)begin
-        rtl_pmem_write(w_mem_addr,w_mem_data,w_mem_len);
-        if(IFU_valid) begin
-            LSU_valid = 1'b1;
-        end
-        else begin
-            LSU_valid = 1'b0;
-        end
-        //r_mem_data_tmp = 32'b00000000;
-        //r_mem_data = 32'b00000000;
-    end
+    assign LSU_valid = (state == ysyx_24120011_M_AXI_RDATA || state == ysyx_24120011_M_AXI_WRESP) ? 1 : 0;
     
-    else if(r_mem_en && !w_mem_en)begin
-        //r_mem_data_tmp = rtl_pmem_read(r_mem_addr);
-        if(IFU_valid) begin
-            LSU_valid = LSU_valid_tmp;
-        end
-        else begin
-            LSU_valid = 1'b0;
-        end
-        if(r_mem_len == 8'd1)begin
-            if(sign_extension)begin
-                r_mem_data = {{24{r_mem_data_tmp[7]}},r_mem_data_tmp[7:0]};
-            end
-            else begin
-                r_mem_data = {24'b0,r_mem_data_tmp[7:0]};
-            end
-        end
-        else if(r_mem_len == 8'd2)begin
-            if(sign_extension)begin
-                r_mem_data = {{16{r_mem_data_tmp[15]}},r_mem_data_tmp[15:0]};
-            end
-            else begin
-                r_mem_data = {16'b0,r_mem_data_tmp[15:0]};
-            end
-        end
-        else if(r_mem_len == 8'd4)begin
-            r_mem_data = r_mem_data_tmp;
-        end
+    //AR
+    assign araddr = (state == ysyx_24120011_M_AXI_RADDR) ? r_mem_addr : 32'b0;
+    assign arvalid = (state == ysyx_24120011_M_AXI_RADDR) ? 1 : 0;
 
-        else begin//shouldn't in
-            r_mem_data = 32'b11111111;
+    //R
+    assign rready = (state == ysyx_24120011_M_AXI_RDATA) ? 1 : 0;
+
+    //AW
+    assign awvalid = (state == ysyx_24120011_M_AXI_WADDR) ? 1 : 0;
+    assign awaddr = (state == ysyx_24120011_M_AXI_WADDR) ? w_mem_addr : 32'b0;
+
+    //W
+    assign wvalid = (state == ysyx_24120011_M_AXI_WDATA) ? 1 : 0;
+    assign wdata = (state == ysyx_24120011_M_AXI_WDATA) ? w_mem_data : 32'b0;
+    assign wstrb = (w_mem_len == 8'd4) ? 
+                    4'b1111 :
+                    ((w_mem_len == 8'd2) ? 4'b0011 : 4'b0001);
+    //B
+    assign bready = (state == ysyx_24120011_M_AXI_WRESP) ? 1 : 0;
+
+    always@(*)begin
+        if(next_state == ysyx_24120011_M_AXI_IDLE)begin
+            LSU_valid = 1;
         end
     end
 
-    else begin
-        if(IFU_valid) begin
-            LSU_valid = 1'b1;
+    always@(posedge clk)begin
+        if(rst) begin
+            r_mem_data <= 32'b0;
         end
         else begin
-            LSU_valid = 1'b0;
+            if(state == ysyx_24120011_M_AXI_RDATA) begin
+                if(r_mem_len == 8'd1)begin
+                    if(sign_extension)begin
+                        r_mem_data = {{24{rdata[7]}},rdata[7:0]};
+                    end
+                    else begin
+                        r_mem_data = {24'b0,rdata[7:0]};
+                    end
+                end
+                else if(r_mem_len == 8'd2)begin
+                    if(sign_extension)begin
+                        r_mem_data = {{16{rdata[15]}},rdata[15:0]};
+                    end
+                    else begin
+                        r_mem_data = {16'b0,rdata[15:0]};
+                    end
+                end
+                else if(r_mem_len == 8'd4)begin
+                    r_mem_data = rdata;
+                end
+                else begin//shouldn't in
+                    r_mem_data = 32'b11111111;
+                end
+            end
         end
-        //r_mem_data_tmp = 32'b11111111;
-        //r_mem_data = 32'b11111111;
     end
-end
+
+    always@(posedge clk)begin
+        if(rst) begin
+            start_read_delay <= 0;
+            start_write_delay <= 0;
+        end
+        else begin
+            if(IFU_valid) begin
+                start_read_delay <= r_mem_en;
+                start_write_delay <= w_mem_en;
+            end
+        end
+    end
+
+    always@(*)begin
+        case(state)
+            ysyx_24120011_M_AXI_IDLE: next_state = (start_read_delay) ? 
+                                                    ysyx_24120011_M_AXI_RADDR : 
+                                                    ((start_write_delay) ? ysyx_24120011_M_AXI_WADDR : ysyx_24120011_M_AXI_IDLE);
+            ysyx_24120011_M_AXI_RADDR: if (arvalid && arready) next_state = ysyx_24120011_M_AXI_RDATA;
+            ysyx_24120011_M_AXI_RDATA: if (rvalid  && rready ) next_state = ysyx_24120011_M_AXI_IDLE;
+            ysyx_24120011_M_AXI_WADDR: if (awvalid && awready) next_state = ysyx_24120011_M_AXI_WDATA;
+            ysyx_24120011_M_AXI_WDATA: if (wvalid  && wready ) next_state = ysyx_24120011_M_AXI_WRESP;
+            ysyx_24120011_M_AXI_WRESP: if (bvalid  && bready ) next_state = ysyx_24120011_M_AXI_IDLE;
+            default : next_state = ysyx_24120011_M_AXI_IDLE;
+        endcase
+    end
+
+    always@(posedge clk)begin
+        if(rst) begin
+            state <= ysyx_24120011_M_AXI_IDLE;
+        end
+        else begin
+            state <= next_state;
+        end
+    end
+
+// always@(posedge clk)begin
+//     if(w_mem_en && !r_mem_en)begin
+//         rtl_pmem_write(w_mem_addr,w_mem_data,w_mem_len);
+//         if(IFU_valid) begin
+//             LSU_valid = 1'b1;
+//         end
+//         else begin
+//             LSU_valid = 1'b0;
+//         end
+//         //r_mem_data_tmp = 32'b00000000;
+//         //r_mem_data = 32'b00000000;
+//     end
+    
+//     else if(r_mem_en && !w_mem_en)begin
+//         //r_mem_data_tmp = rtl_pmem_read(r_mem_addr);
+//         if(IFU_valid) begin
+//             LSU_valid = LSU_valid_tmp;
+//         end
+//         else begin
+//             LSU_valid = 1'b0;
+//         end
+//         if(r_mem_len == 8'd1)begin
+//             if(sign_extension)begin
+//                 r_mem_data = {{24{r_mem_data_tmp[7]}},r_mem_data_tmp[7:0]};
+//             end
+//             else begin
+//                 r_mem_data = {24'b0,r_mem_data_tmp[7:0]};
+//             end
+//         end
+//         else if(r_mem_len == 8'd2)begin
+//             if(sign_extension)begin
+//                 r_mem_data = {{16{r_mem_data_tmp[15]}},r_mem_data_tmp[15:0]};
+//             end
+//             else begin
+//                 r_mem_data = {16'b0,r_mem_data_tmp[15:0]};
+//             end
+//         end
+//         else if(r_mem_len == 8'd4)begin
+//             r_mem_data = r_mem_data_tmp;
+//         end
+
+//         else begin//shouldn't in
+//             r_mem_data = 32'b11111111;
+//         end
+//     end
+
+//     else begin
+//         if(IFU_valid) begin
+//             LSU_valid = 1'b1;
+//         end
+//         else begin
+//             LSU_valid = 1'b0;
+//         end
+//         //r_mem_data_tmp = 32'b11111111;
+//         //r_mem_data = 32'b11111111;
+//     end
+// end
 endmodule
