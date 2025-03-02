124dba486359a95a9673ea404852a26285bc5c00
tracer-ysyx <tracer@ysyx.org>
2025-03-02 13:26:14 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:26:14 up 2 days,  4:46,  1 user,  load average: 0.65, 0.35, 0.45

--- npc/vsrc/ysyx_24120011_SRAM.v
+++ npc/vsrc/ysyx_24120011_SRAM.v
@@ -9,7 +9,7 @@ module SRAM (
     //R//
     output reg [31:0] rdata,
     output [1:0]  rresp,
-    output reg       rvalid,
+    output        rvalid,
     input         rready,
     //AW//
     input  [31:0] awaddr,
@@ -47,16 +47,16 @@ module SRAM (
     always@(posedge clk)begin
         if(state == ysyx_24120011_S_AXI_RDATA)begin
             rdata <= rtl_pmem_read(addr);
-            rvalid <= 1;
+            //rvalid <= 1;
         end
         else begin
             rdata <= rdata;
-            rvalid <= 0;
+            //rvalid <= 0;
         end
     end
 
 	assign rresp  = ysyx_24120011_S_AXI_RESP_OKAY;
-	//assign rvalid = (state == ysyx_24120011_S_AXI_RDATA) ? 1 : 0;
+	assign rvalid = (state == ysyx_24120011_S_AXI_RDATA) ? 1 : 0;
 
 	// AW
 	assign awready = (state == ysyx_24120011_S_AXI_WADDR) ? 1 : 0;
