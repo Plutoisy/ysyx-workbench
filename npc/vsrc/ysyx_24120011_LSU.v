6fb5b68103d5cfc8f169745bd432c677c9d6daf0
tracer-ysyx <tracer@ysyx.org>
2025-03-03 21:30:07 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:30:07 up  3:36,  1 user,  load average: 0.16, 0.13, 0.10

--- npc/vsrc/ysyx_24120011_LSU.v
+++ npc/vsrc/ysyx_24120011_LSU.v
@@ -99,13 +99,13 @@ module ysyx_24120011_LSU(
 /* verilator lint_off LATCH */
 
     always@(posedge clk)begin
-        if(state == ysyx_24120011_M_AXI_RADDR && arvalid_delay_cnt != 32'd0)begin
+        if(state == ysyx_24120011_M_AXI_RADDR && arvalid_delay_cnt != 32'd0 && arvalid_delay_cnt != 32'hFFFFFFFF)begin
             arvalid_delay_cnt <= arvalid_delay_cnt - 1;
             arvalid <= 0;
         end
         else if(state == ysyx_24120011_M_AXI_RADDR && arvalid_delay_cnt == 32'd0)begin
             arvalid <= 1;
-            arvalid_delay_cnt <= 32'b11111111111111111111111111111111;
+            arvalid_delay_cnt <= 32'hFFFFFFFF
         end
         else if(state == ysyx_24120011_M_AXI_RADDR && arready == 1)begin
             arvalid <= 0;
