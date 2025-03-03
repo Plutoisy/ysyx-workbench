57837bf70b7bdc93da1bc9dce3371cdbe1f01126
tracer-ysyx <tracer@ysyx.org>
2025-03-03 20:04:04 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:04:04 up  2:10,  1 user,  load average: 0.54, 0.40, 0.21

--- npc/vsrc/ysyx_24120011_SRAM.v
+++ npc/vsrc/ysyx_24120011_SRAM.v
@@ -125,7 +125,7 @@ module SRAM (
     always@(posedge clk)begin
         if(rst)begin
             state <= ysyx_24120011_S_AXI_IDLE;
-            read_dalay <= 32'd0;
+            read_dalay <= 32'd7;
         end
         else begin
             state <= next_state;
