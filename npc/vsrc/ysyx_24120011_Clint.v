3a28f211d4937802e172c33b36467e2370c5903b
tracer-ysyx <tracer@ysyx.org>
2025-03-08 17:42:46 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:42:46 up 11:51,  1 user,  load average: 0.05, 0.04, 0.07

--- npc/vsrc/ysyx_24120011_Clint.v
+++ npc/vsrc/ysyx_24120011_Clint.v
@@ -111,13 +111,13 @@ module ysyx_24120011_Clint (
         end
         else if(state == ysyx_24120011_S_AXI_WDATA && write_delay_cnt == 0 && pmem_writed == 0)begin
             if(wstrb == 4'b1111) begin
-                //rtl_pmem_write(addr,wdata,4);
+                rtl_pmem_write(addr,wdata,4);
             end
             else if(wstrb == 4'b0011) begin
-                //rtl_pmem_write(addr,wdata,2);
+                rtl_pmem_write(addr,wdata,2);
             end
             else if(wstrb == 4'b0001) begin
-                //rtl_pmem_write(addr,wdata,1);
+                rtl_pmem_write(addr,wdata,1);
             end
             else ;
             wready_reg <= 1;
