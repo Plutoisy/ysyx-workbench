3b1ded403f1dd853aab35ab360323f815495edfa
tracer-ysyx <tracer@ysyx.org>
2025-03-08 16:55:51 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:55:51 up 11:04,  1 user,  load average: 0.08, 0.13, 0.16

--- npc/vsrc/ysyx_24120011_Uart.v
+++ npc/vsrc/ysyx_24120011_Uart.v
@@ -118,7 +118,7 @@ module ysyx_24120011_Uart (
                     $write("%c", wdata[7:0]);
                 end
                 else if(wstrb == 4'b0001) begin
-                    $write("%h", {4'b0,wdata[3:0]});
+                    $write("%C", {4'b0,wdata[3:0]});
                 end
                 else ;
             end
