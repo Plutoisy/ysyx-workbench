4ee11268e99d6bbf8fd63b42641713ab5bda885f
tracer-ysyx <tracer@ysyx.org>
2025-03-09 19:51:22 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:51:22 up 14:25,  1 user,  load average: 0.62, 0.33, 0.18

--- npc/obj_dir/Vysyx_24120011_top__Syms.cpp
+++ npc/obj_dir/Vysyx_24120011_top__Syms.cpp
@@ -20,8 +20,8 @@ Vysyx_24120011_top__Syms::Vysyx_24120011_top__Syms(VerilatedContext* contextp, c
     , TOP____024unit{this, Verilated::catName(namep, "$unit")}
 {
     // Configure time unit / time precision
-    _vm_contextp__->timeunit(-15);
-    _vm_contextp__->timeprecision(-15);
+    _vm_contextp__->timeunit(-12);
+    _vm_contextp__->timeprecision(-12);
     // Setup each module's pointers to their submodules
     TOP.__PVT____024unit = &TOP____024unit;
     // Setup each module's pointer back to symbol table (for public functions)
