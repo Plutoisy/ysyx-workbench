c0d5e1f13e8827f4c33c9cf377ffefc19ae8b39c
tracer-ysyx <tracer@ysyx.org>
2025-03-11 13:16:10 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:16:10 up 1 day,  8:10,  1 user,  load average: 0.40, 0.30, 0.15

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -233,7 +233,7 @@ extern "C" void mrom_read(int32_t addr, int32_t *data) {
     return;
   }
   else{
-    *data = host_read(guest_to_host_soc(addr), 4);
+    *data = (int32_t)host_read(guest_to_host_soc(addr), 4);
     printf("R->data: 0x%x, len: %d\n", pmem[addr-CONFIG_MBASE_SOC], 4);
     return;
   }
