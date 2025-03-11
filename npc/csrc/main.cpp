82ae3902297738ad922c72e4b35d57624ffbaf19
tracer-ysyx <tracer@ysyx.org>
2025-03-11 11:57:50 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:57:50 up 1 day,  6:51,  1 user,  load average: 0.23, 0.19, 0.12

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -12,6 +12,7 @@
 
 #define PMEM_SIZE    0x8000000
 #define CONFIG_MBASE 0x80000000
+#define CONFIG_MBASE_SOC 0x20000000
 #define ARRLEN(arr) (int)(sizeof(arr) / sizeof(arr[0]))
 #define M_R_TRACE 0
 #define M_W_TRACE 0
@@ -170,6 +171,7 @@ void system_rst(){
 }
 
 uint8_t* guest_to_host(uint32_t paddr) { return pmem + paddr - CONFIG_MBASE; }
+uint8_t* guest_to_host_soc(uint32_t paddr) { return pmem + paddr - CONFIG_MBASE_SOC; }
 
 static inline uint32_t host_read(void *addr, int len) {
   switch (len) {
@@ -221,7 +223,19 @@ void isa_reg_display() {
 
 extern "C" void flash_read(int32_t addr, int32_t *data) { assert(0); }
 extern "C" void mrom_read(int32_t addr, int32_t *data) { 
-  *data = 0x00100073;
+  if(addr - CONFIG_MBASE_SOC > 0xfff){
+    if(M_R_TRACE){
+      printf("R->addr: 0x%x, len: %d\n", r_mem_addr, 4);
+    }
+    if(M_R_ASSERT){
+      assert(0);
+    }
+    return 0;
+  }
+  else{
+    uint32_t *data = host_read(guest_to_host_soc(addr), 4);
+    return;
+  }
 }
 
 extern "C" void ebreak(){
