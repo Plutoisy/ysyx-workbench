ed55a429d59de17709db59955b34253442dd5424
tracer-ysyx <tracer@ysyx.org>
2025-03-12 13:21:14 +0800

    > compile NEMU ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:21:13 up 1 day, 18:28,  1 user,  load average: 0.62, 0.63, 0.46

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -41,11 +41,13 @@ static void pmem_write(paddr_t addr, int len, word_t data) {
 }
 
 static word_t sram_read(paddr_t addr, int len) {
+  printf("R->addr: 0x%x, len: %d, mem: 0x%08x\n", addr, len, sram_read(addr,len));
   word_t ret = host_read(guest_to_host_sram(addr), len);
   return ret;
 }
 
 static void sram_write(paddr_t addr, int len, word_t data) {
+  printf("W->addr: 0x%x, len: %d, mem: 0x%08x\n", addr, len, data);
   host_write(guest_to_host_sram(addr), len, data);
 }
 
@@ -79,7 +81,7 @@ void paddr_write(paddr_t addr, int len, word_t data) {
 }
 
 word_t sramaddr_read(paddr_t addr, int len) {
-  printf("R->addr: 0x%x, len: %d, mem: 0x%08x\n", addr, len, pmem_read(addr,len));
+  IFDEF(CONFIG_MTRACE, printf("R->addr: 0x%x, len: %d, mem: 0x%08x\n", addr, len, sram_read(addr,len)));
   if (likely(in_sram(addr))) return sram_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
@@ -87,7 +89,7 @@ word_t sramaddr_read(paddr_t addr, int len) {
 }
 
 void sramaddr_write(paddr_t addr, int len, word_t data) {
-  printf("W->addr: 0x%x, len: %d, mem: 0x%08x\n", addr, len, data);
+  IFDEF(CONFIG_MTRACE, printf("W->addr: 0x%x, len: %d, mem: 0x%08x\n", addr, len, data));
   if (likely(in_sram(addr))) { sram_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
