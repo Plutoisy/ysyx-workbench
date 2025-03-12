96b99c6ccada28e0f921327b404e186b0855b8d3
tracer-ysyx <tracer@ysyx.org>
2025-03-12 13:14:01 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:14:01 up 1 day, 18:21,  1 user,  load average: 1.14, 0.56, 0.37

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -18,8 +18,8 @@
 #define M_W_TRACE 0
 #define M_R_ASSERT 1
 #define M_W_ASSERT 1
-#define PC_ASSERT 0
-#define REG_ASSERT 0
+#define PC_ASSERT 1
+#define REG_ASSERT 1
 #define DIFFTESE 1
 #define BMODE 1
 #define WAVE 0
