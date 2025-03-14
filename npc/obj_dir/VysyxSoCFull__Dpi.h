e9d74e2b25cf94923cec813855c132fae397f7be
tracer-ysyx <tracer@ysyx.org>
2025-03-14 22:26:50 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:26:50 up 14:48,  1 user,  load average: 0.32, 0.39, 0.37

---
+++ npc/obj_dir/VysyxSoCFull__Dpi.h
@@ -0,0 +1,40 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Prototypes for DPI import and export functions.
+//
+// Verilator includes this file in all generated .cpp files that use DPI functions.
+// Manually include this file where DPI .c import functions are declared to ensure
+// the C functions match the expectations of the DPI imports.
+
+#ifndef VERILATED_VYSYXSOCFULL__DPI_H_
+#define VERILATED_VYSYXSOCFULL__DPI_H_  // guard
+
+#include "svdpi.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+    // DPI IMPORTS
+    // DPI import at /home/plutoisy/ysyx-workbench/npc/vsrc/ysyx_24120011.v:2:30
+    extern void ebreak();
+    // DPI import at /home/plutoisy/ysyx-workbench/ysyxSoC/perip/flash/flash.v:84:30
+    extern void flash_read(int addr, int* data);
+    // DPI import at /home/plutoisy/ysyx-workbench/npc/vsrc/ysyx_24120011.v:4:30
+    extern void get_pc_inst(int pc, int dnpc, int inst, int IFU_valid_int);
+    // DPI import at /home/plutoisy/ysyx-workbench/ysyxSoC/build/ysyxSoCFull.v:4617:30
+    extern void mrom_read(int raddr, int* rdata);
+    // DPI import at /home/plutoisy/ysyx-workbench/npc/vsrc/ysyx_24120011.v:3:30
+    extern void npc_trap(int pc, int ret);
+    // DPI import at /home/plutoisy/ysyx-workbench/npc/vsrc/ysyx_24120011_RegStack.v:2:30
+    extern void reg_out(const int* array);
+    // DPI import at /home/plutoisy/ysyx-workbench/npc/vsrc/ysyx_24120011_LSU.v:3:29
+    extern int rtl_pmem_read(int r_mem_addr);
+    // DPI import at /home/plutoisy/ysyx-workbench/npc/vsrc/ysyx_24120011_LSU.v:2:30
+    extern void rtl_pmem_write(int w_mem_addr, int w_mem_data, char w_mem_len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // guard
