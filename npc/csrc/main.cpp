62fc34aca3b7e3667975c3788306f84fbcc929da
tracer-ysyx <tracer@ysyx.org>
2025-03-15 23:33:06 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:33:06 up 1 day, 8 min,  1 user,  load average: 1.17, 0.75, 0.50

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -243,7 +243,7 @@ void isa_reg_display() {
 }
 
 extern "C" void flash_read(int32_t addr, int32_t *data) { 
-  assert(0);
+  printf("flashR->addr: 0x%08x, len: %d, mem: 0x%08x\n", addr & ~0x3, 4, *data);
   // if(addr - CONFIG_FLASHBASE > FLASH_SIZE){
   //   if(M_R_ASSERT){
   //     assert(0);
