ac8a9002289eb160b124f9cf9e4cf81b81354292
tracer-ysyx <tracer@ysyx.org>
2025-03-16 18:19:04 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:19:04 up  6:31,  1 user,  load average: 0.54, 0.51, 0.37

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -127,7 +127,7 @@ static long load_img() {
   return size;
 }
 
-static long load_img_to_flash(char* img) {
+static long load_img_to_flash(char *img) {
   if (img == NULL) {
     printf("No image is given. Use the default build-in image.\n");
     return 4096; // built-in image size
@@ -607,7 +607,7 @@ int main(int argc, char *argv[]) {
 
   load_img();
   if(LOAD_FLASH_IMG){
-    load_img_to_flash("/home/plutoisy/ysyx-workbench/npc/npc_test/build/char-test.bin");
+    load_img_to_flash("/home/plutoisy/ysyx-workbench/npc/npc_test/build/char_test.bin");
   }
   if(DIFFTESE){
     difftest_memcpy(CONFIG_MBASE_SOC, pmem, PMEM_SIZE_SOC, 1);
