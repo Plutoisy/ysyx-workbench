ad874671f2aba78ba769fd6ddac2417a569c2a2d
tracer-ysyx <tracer@ysyx.org>
2025-03-19 00:20:43 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:20:43 up 17:46,  1 user,  load average: 0.11, 0.12, 0.10

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -13,20 +13,27 @@
 #define UART_REG_LSR     0x10000005L
 
 extern char _heap_start;
+extern char _psram_end;
 int main(const char *args);
 
-extern char _pmem_start;
-#define PMEM_SIZE (0xfff)
-#define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
+// extern char _mrom_start;
+// #define MROM_SIZE (4 * 1024)
+// #define MROM_END  ((uintptr_t)&_mrom_start + MROM_SIZE)
 
-extern char _data_lma_start,_data_vma_start,_bss_start;
+extern char _sram_start;
+#define SRAM_SIZE (8 * 1024)
+#define SRAM_END ((uintptr_t)&_sram_start + SRAM_SIZE)
 
-extern char _ssbl_origin,_ssbl_ram_start,_ssbl_ram_end;
-extern char _test_origin,_text_ram_start,_data_ram_end;
-extern char _bss_start,_bss_end;
+// extern char _flash_start;
+// #define FLASH_SIZE (256 * 1024 * 1024)
+// #define FLASH_END ((uintptr_t)&_flash_start + FLASH_SIZE)
 
-Area heap = RANGE(&_heap_start, &_heap_start + 0xfff);
-static const char mainargs[MAINARGS_MAX_LEN] = MAINARGS_PLACEHOLDER; // defined in CFLAGS
+
+Area heap = RANGE(&_heap_start, &_psram_end);
+#ifndef MAINARGS
+#define MAINARGS ""
+#endif
+static const char mainargs[] = MAINARGS;
 
 void putch(char ch) {
   while (!(inb(UART_REG_LSR) & 0x20)) {
@@ -38,29 +45,7 @@ void halt(int code) {
   asm volatile("mv a0, %0; ebreak" : :"r"(code));
   while (1);
 }
-void fsbl(){
-  // boot_memcpy((uint32_t)&_ssbl_origin,(uint32_t)&_ssbl_ram_start,(uint32_t)&_ssbl_ram_end);
-  volatile uint32_t *src = (volatile uint32_t *)&_ssbl_origin;
-  volatile uint32_t *dst = (volatile uint32_t *)&_ssbl_ram_start;
-  while((uint32_t) dst < (uint32_t) &_ssbl_ram_end){
-    *dst++ = *src++;
-  }
-  
-}
-void ssbl(){
-  // boot_memcpy((uint32_t)&_test_origin,(uint32_t)&_text_ram_start,(uint32_t)&_data_ram_end);
-  volatile uint32_t *src = (volatile uint32_t *)&_test_origin;
-  volatile uint32_t *dst = (volatile uint32_t *)&_text_ram_start;
-  while((uint32_t) dst < (uint32_t) &_data_ram_end){
-    *dst++ = *src++;
-  }
-}
-void bss_init(){
-  volatile uint32_t *src = (volatile uint32_t *)&_bss_start;
-  while((uint32_t) src < (uint32_t) &_bss_end){
-    *src++ = 0;
-  }
-}
+
 void _trm_init() {
   //memcpy(&_data_vma_start,&_data_lma_start,&_bss_start-&_data_vma_start);
   //fsbl();
