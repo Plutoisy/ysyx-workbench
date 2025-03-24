e5573b080b254258108094888308455343e6a53e
tracer-ysyx <tracer@ysyx.org>
2025-03-24 14:31:44 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:31:44 up 22:29,  1 user,  load average: 0.48, 0.54, 0.64

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -1,104 +1,67 @@
 #include <am.h>
 #include <klib-macros.h>
 #include <riscv/riscv.h>
-#include <assert.h>
 #include <string.h>
 #include <stdio.h>
-#define DEVICE_BASE 0x10000000
-#define MMIO_BASE 0xE0000000
 
-#define SERIAL_PORT         0x10000000
+#define UART_BASE 0x10000000L
+#define UART_TX   0
+#define SERIAL_PORT     (UART_BASE + UART_TX)
+#define UART_REG_LC      0x10000003L
+#define UART_REG_DL1     0x10000000L
+#define UART_REG_DL2     0x10000001L
+#define UART_REG_LSR     0x10000005L
 
-extern char _heap_start,_heap_end,_rodata_end,_bss_end,_text_start;
+extern char _heap_start;
+extern char _psram_end;
+
 int main(const char *args);
 
-extern char _pmem_start;
-#define PMEM_SIZE 0x0fffffff
-#define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
+extern char _sram_start;
+#define SRAM_SIZE (8 * 1024)
+#define SRAM_END ((uintptr_t)&_sram_start + SRAM_SIZE)
 
-Area heap = RANGE(&_heap_start, &_heap_end);
+Area heap = RANGE(&_heap_start, &_psram_end);
 #ifndef MAINARGS
 #define MAINARGS ""
 #endif
 static const char mainargs[] = MAINARGS;
 
 
-extern char _ssbl_origin,_ssbl_ram_start,_ssbl_ram_end;
-extern char _test_origin,_text_ram_start,_data_ram_end;
-extern char _bss_start,_bss_end;
-uint32_t number;
-
 void putch(char ch) {
-  //这里要加东西
-  while((inb(SERIAL_PORT+5)&0x20) == 0){
-    
+  while (!(inb(UART_REG_LSR) & 0x20)) {
   }
   outb(SERIAL_PORT, ch);
 }
-void print_char(uint32_t){
-  putch((number&0xff000000) >> 24);
-  putch((number&0x00ff0000) >> 16);
-  putch((number&0x0000ff00) >> 8);
-  putch((number&0x000000ff) >> 0);
-}
 
 void halt(int code) {
-  // printf("finished\n");
-  // from #include <nemu.h>
   asm volatile("mv a0, %0; ebreak" : :"r"(code));
-
   while (1);
 }
-void uart_init(){
-  outb(SERIAL_PORT+3,0x80); //设置LCR i 7th DLAB 为1
-  outb(SERIAL_PORT  ,0x01);
-  outb(SERIAL_PORT+1,0x00);
-  outb(SERIAL_PORT+3,0x03); // 重新设置LCR
-  outb(SERIAL_PORT+2,0x01); // FCR寄存器，
-  outb(SERIAL_PORT+1,0x01); // 使能接收中断
-}
-#pragma GCC push_options
-#pragma GCC optimize("O0")
-void read_csr(){
-  asm volatile("csrr %0, %1" : "=r"(number) : "i"(0xF11));
-  // print_char(number);
-  asm volatile("csrr %0, %1" : "=r"(number) : "i"(0xF12));
-  // printf("%d\n",number);
-}
-void ssbl();
-void device_init();
-void fsbl(){
-  // boot_memcpy((uint32_t)&_ssbl_origin,(uint32_t)&_ssbl_ram_start,(uint32_t)&_ssbl_ram_end);
-  volatile uint32_t *src = (volatile uint32_t *)&_ssbl_origin;
-  volatile uint32_t *dst = (volatile uint32_t *)&_ssbl_ram_start;
-  while((uint32_t) dst < (uint32_t) &_ssbl_ram_end){
-    *dst++ = *src++;
-  }
-  ssbl();
-}
-void ssbl(){
-  // copy text rodata data 
-  volatile uint32_t *src = (volatile uint32_t *)&_test_origin;
-  volatile uint32_t *dst = (volatile uint32_t *)&_text_ram_start;
-  while((uint32_t) dst < (uint32_t) &_data_ram_end){
-    *dst++ = *src++;
-  }
-  // init_bss
-  src = (volatile uint32_t *)&_bss_start;
-  while((uint32_t) src < (uint32_t) &_bss_end){
-    *src++ = 0;
-  }
-  device_init();
+
+void uart_init() {
+  int divisor = 1;
+  outb(UART_REG_LC, inb(UART_REG_LC) | 0x80);
+  outb(UART_REG_DL2, (divisor >> 8) & 0xFF);
+  outb(UART_REG_DL1, divisor & 0xFF);
+  outb(UART_REG_LC, inb(UART_REG_LC) & ~0x80);
 }
 
-void device_init(){
+void print_csr() {
+  uint32_t mvendorid, marchid;
+  asm volatile ("csrr %0, mvendorid" : "=r" (mvendorid): : );
+  asm volatile ("csrr %0, marchid" : "=r" (marchid) : : );
+  printf("mvendorid = %c%c%c%c\n",
+    (char)(mvendorid >> 24),
+    (char)(mvendorid >> 16),
+    (char)(mvendorid >> 8),
+    (char)mvendorid);
+  printf("marchid   = %d\n", marchid);
+}
+
+void _trm_init() {
   uart_init();
-  putch('f');
-  putch('\n');
-  // read_csr();
+  print_csr();
   int ret = main(mainargs);
   halt(ret);
-}
-void _trm_init() {
-  fsbl();
 }
