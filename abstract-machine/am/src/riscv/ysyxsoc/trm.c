24c5689e238478c4b0be0fec83b78e4af396ce0b
tracer-ysyx <tracer@ysyx.org>
2025-03-24 13:34:45 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:34:45 up 21:32,  1 user,  load average: 0.88, 0.71, 0.55

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -14,11 +14,11 @@
 
 extern char _heap_start;
 extern char _psram_end;
-extern char _bl_s, _bl_s_load, _ebl_s;
-extern char _text, _text_load, _etext;
-extern char _data, _data_load, _edata;
-extern char _data_extra, _data_extra_load, _edata_extra;
-extern char _bss_start, _ebss;
+extern char _bl_s[], _bl_s_load[], _ebl_s[];
+extern char _text[], _text_load[], _etext[];
+extern char _data[], _data_load[], _edata[];
+extern char _data_extra[], _data_extra_load[], _edata_extra[];
+extern char _bss_start[], _ebss[];
 
 int main(const char *args);
 
@@ -64,11 +64,37 @@ void print_csr() {
   printf("marchid   = %d\n", marchid);
 }
 
+void _bl_ss_load_align4(char *dest, char *src, char *dest_end) {
+  // 计算大小
+  unsigned int size = (unsigned int)(dest_end - dest);
+  
+  // 如果大小为0，直接返回
+  if (size == 0) {
+      return;
+  }
+  
+  // 以4字节为单位复制内存
+  for (unsigned int i = 0; i < size; i += 4) {
+      // 在C中，我们不能像汇编那样直接操作4字节，所以使用指针转换
+      *((unsigned int*)(dest + i)) = *((unsigned int*)(src + i));
+  }
+}
+
 void ssbl() {
-  memcpy(&_text, &_text_load, (&_etext - &_text));
-  memcpy(&_data, &_data_load, (&_edata - &_data));
-  memcpy(&_data_extra, &_data_extra_load, (&_edata_extra - &_data_extra));
-  memset(&_bss_start, 0, (&_ebss - &_bss_start));
+  // 复制.text段
+  _bl_ss_load_align4(_text, _text_load, _etext);
+    
+  // 复制.data段
+  _bl_ss_load_align4(_data, _data_load, _edata);
+  
+  // 复制额外数据段
+  _bl_ss_load_align4(_data_extra, _data_extra_load, _edata_extra);
+  
+  // 初始化.bss段(清零)
+  unsigned int bss_size = (unsigned int)(_ebss - _bss_start);
+  for (unsigned int i = 0; i < bss_size; i += 4) {
+      *((unsigned int*)(_bss_start + i)) = 0;
+  }
 
   uart_init();
   print_csr();
@@ -77,7 +103,14 @@ void ssbl() {
 }
 
 void fsbl() {
-  memcpy(&_bl_s, &_bl_s_load, (&_ebl_s - &_bl_s));
+  char *dest = _bl_s;
+  char *src = _bl_s_load;
+  unsigned int size = (unsigned int)(_ebl_s - _bl_s);
+  
+  for (unsigned int i = 0; i < size; i += 4) {
+      *((unsigned int*)(dest + i)) = *((unsigned int*)(src + i));
+  }
+
   ssbl();
 }
 
