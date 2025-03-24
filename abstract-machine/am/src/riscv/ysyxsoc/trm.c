47ef0ee5fc7a8fc7f48bcf04d7809b24a7c70c9f
tracer-ysyx <tracer@ysyx.org>
2025-03-24 12:18:47 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:18:47 up 20:16,  1 user,  load average: 0.52, 0.58, 0.57

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -14,20 +14,18 @@
 
 extern char _heap_start;
 extern char _psram_end;
-int main(const char *args);
+extern char *_bl_s, *_bl_s_load, *_ebl_s;
+extern char *_text, *_text_load, *_etext;
+extern char *_data, *_data_load, *_edata;
+extern char *_data_extra, *_data_extra_load, *_edata_extra;
+extern char *_bss_start, *_ebss;
+extern char *_stack_pointer;
 
-// extern char _mrom_start;
-// #define MROM_SIZE (4 * 1024)
-// #define MROM_END  ((uintptr_t)&_mrom_start + MROM_SIZE)
+int main(const char *args);
 
 extern char _sram_start;
 #define SRAM_SIZE (8 * 1024)
 #define SRAM_END ((uintptr_t)&_sram_start + SRAM_SIZE)
-
-// extern char _flash_start;
-// #define FLASH_SIZE (256 * 1024 * 1024)
-// #define FLASH_END ((uintptr_t)&_flash_start + FLASH_SIZE)
-
 
 Area heap = RANGE(&_heap_start, &_psram_end);
 #ifndef MAINARGS
@@ -67,7 +65,52 @@ void print_csr() {
   printf("marchid   = %d\n", marchid);
 }
 
+void _bl_ss_load_align4(char *dest, char *src, char *dest_end) {
+  // 计算大小
+  unsigned int size = (unsigned int)(dest_end - dest);
+  
+  // 如果大小为0，直接返回
+  if (size == 0) {
+      return;
+  }
+  
+  // 以4字节为单位复制内存
+  for (unsigned int i = 0; i < size; i += 4) {
+      // 在C中，我们不能像汇编那样直接操作4字节，所以使用指针转换
+      *((unsigned int*)(dest + i)) = *((unsigned int*)(src + i));
+  }
+}
+
+void fsbl() {
+  char *dest = _bl_s;
+  char *src = _bl_s_load;
+  unsigned int size = (unsigned int)(_ebl_s - _bl_s);
+  
+  for (unsigned int i = 0; i < size; i += 4) {
+      *((unsigned int*)(dest + i)) = *((unsigned int*)(src + i));
+  }
+}
+
+void ssbl() {
+  // 复制.text段
+  _bl_ss_load_align4(_text, _text_load, _etext);
+    
+  // 复制.data段
+  _bl_ss_load_align4(_data, _data_load, _edata);
+  
+  // 复制额外数据段
+  _bl_ss_load_align4(_data_extra, _data_extra_load, _edata_extra);
+  
+  // 初始化.bss段(清零)
+  unsigned int bss_size = (unsigned int)(_ebss - _bss_start);
+  for (unsigned int i = 0; i < bss_size; i += 4) {
+      *((unsigned int*)(_bss_start + i)) = 0;
+  }
+}
+
 void _trm_init() {
+  fsbl();
+  ssbl();
   uart_init();
   print_csr();
   int ret = main(mainargs);
