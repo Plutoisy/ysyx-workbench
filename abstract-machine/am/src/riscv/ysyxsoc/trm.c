a578f40e9cbeac137e6613f9393d9907a9973d1d
tracer-ysyx <tracer@ysyx.org>
2025-03-24 14:07:27 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:07:27 up 22:05,  1 user,  load average: 0.58, 0.54, 0.61

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -12,25 +12,23 @@
 #define UART_REG_DL2     0x10000001L
 #define UART_REG_LSR     0x10000005L
 
-extern char _heap_start;
-extern char _psram_end;
-extern char _bl_s[], _bl_s_load[], _ebl_s[];
-extern char _text[], _text_load[], _etext[];
-extern char _data[], _data_load[], _edata[];
-extern char _data_extra[], _data_extra_load[], _edata_extra[];
-extern char _bss_start[], _ebss[];
-
+extern char _heap_start,_heap_end,_rodata_end,_bss_end,_text_start;
 int main(const char *args);
 
-extern char _sram_start;
-#define SRAM_SIZE (8 * 1024)
-#define SRAM_END ((uintptr_t)&_sram_start + SRAM_SIZE)
+extern char _pmem_start;
+#define PMEM_SIZE 0x0fffffff
+#define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
 
-Area heap = RANGE(&_heap_start, &_psram_end);
+Area heap = RANGE(&_heap_start, &_heap_end);
 #ifndef MAINARGS
 #define MAINARGS ""
 #endif
 static const char mainargs[] = MAINARGS;
+
+
+extern char _ssbl_origin,_ssbl_ram_start,_ssbl_ram_end;
+extern char _test_origin,_text_ram_start,_data_ram_end;
+extern char _bss_start,_bss_end;
 
 
 void putch(char ch) {
@@ -44,15 +42,13 @@ void halt(int code) {
   while (1);
 }
 
-void uart_init() {
+void device_init() {
   int divisor = 1;
   outb(UART_REG_LC, inb(UART_REG_LC) | 0x80);
   outb(UART_REG_DL2, (divisor >> 8) & 0xFF);
   outb(UART_REG_DL1, divisor & 0xFF);
   outb(UART_REG_LC, inb(UART_REG_LC) & ~0x80);
-}
 
-void print_csr() {
   uint32_t mvendorid, marchid;
   asm volatile ("csrr %0, mvendorid" : "=r" (mvendorid): : );
   asm volatile ("csrr %0, marchid" : "=r" (marchid) : : );
@@ -62,58 +58,33 @@ void print_csr() {
     (char)(mvendorid >> 8),
     (char)mvendorid);
   printf("marchid   = %d\n", marchid);
-}
 
-void ssbl() {
-  char *dest;
-  char *src;
-  unsigned int size;
-  // 复制.text段
-  dest = _text;
-  src = _text_load;
-  size = (unsigned int)(_etext - _text);
-  
-  for (unsigned int i = 0; i < size; i += 4) {
-      *((unsigned int*)(dest + i)) = *((unsigned int*)(src + i));
-  }
-    
-  // 复制.data段
-  dest = _data;
-  src = _data_load;
-  size = (unsigned int)(_edata - _data);
-  for (unsigned int i = 0; i < size; i += 4) {
-    *((unsigned int*)(dest + i)) = *((unsigned int*)(src + i));
-  }
-  
-  // 复制额外数据段
-  dest = _data_extra;
-  src = _data_extra_load;
-  size = (unsigned int)(_edata_extra - _data_extra);
-  for (unsigned int i = 0; i < size; i += 4) {
-    *((unsigned int*)(dest + i)) = *((unsigned int*)(src + i));
-  }
-  
-  // 初始化.bss段(清零)
-  unsigned int bss_size = (unsigned int)(_ebss - _bss_start);
-  for (unsigned int i = 0; i < bss_size; i += 4) {
-      *((unsigned int*)(_bss_start + i)) = 0;
-  }
-
-  uart_init();
-  print_csr();
   int ret = main(mainargs);
   halt(ret);
 }
 
-void fsbl() {
-  char *dest = _bl_s;
-  char *src = _bl_s_load;
-  unsigned int size = (unsigned int)(_ebl_s - _bl_s);
-  
-  for (unsigned int i = 0; i < size; i += 4) {
-      *((unsigned int*)(dest + i)) = *((unsigned int*)(src + i));
+void ssbl(){
+  // copy text rodata data 
+  volatile uint32_t *src = (volatile uint32_t *)&_test_origin;
+  volatile uint32_t *dst = (volatile uint32_t *)&_text_ram_start;
+  while((uint32_t) dst < (uint32_t) &_data_ram_end){
+    *dst++ = *src++;
   }
+  // init_bss
+  src = (volatile uint32_t *)&_bss_start;
+  while((uint32_t) src < (uint32_t) &_bss_end){
+    *src++ = 0;
+  }
+  device_init();
+}
 
+void fsbl(){
+  // boot_memcpy((uint32_t)&_ssbl_origin,(uint32_t)&_ssbl_ram_start,(uint32_t)&_ssbl_ram_end);
+  volatile uint32_t *src = (volatile uint32_t *)&_ssbl_origin;
+  volatile uint32_t *dst = (volatile uint32_t *)&_ssbl_ram_start;
+  while((uint32_t) dst < (uint32_t) &_ssbl_ram_end){
+    *dst++ = *src++;
+  }
   ssbl();
 }
 
