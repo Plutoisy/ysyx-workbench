c7e971927c8e4823186beec8c1c7b4d9c97f5e89
tracer-ysyx <tracer@ysyx.org>
2025-03-26 22:56:57 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:56:57 up 10:19,  1 user,  load average: 0.36, 0.30, 0.30

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -90,7 +90,7 @@ void halt(int code) {
 }
 
 void uart_init() {
-  uint16_t divisor = 16;
+  uint16_t divisor =  50000000/(16 * 115200);
   outb(UART_REG_LC, inb(UART_REG_LC) | 0x80);
   outb(UART_REG_DL2, (divisor >> 8) & 0xFF);
   outb(UART_REG_DL1, divisor & 0xFF);
