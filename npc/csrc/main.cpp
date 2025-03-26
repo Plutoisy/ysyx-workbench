527c6d8ee9d072403a2933e0516fd654544d79cf
tracer-ysyx <tracer@ysyx.org>
2025-03-26 15:38:39 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:38:39 up  3:01,  1 user,  load average: 0.73, 0.52, 0.34

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -29,9 +29,9 @@
 #define PC_ASSERT 1
 #define REG_ASSERT 1
 #define DIFFTESE 0
-#define BMODE 1
+#define BMODE 0
 #define WAVE 0
-#define NVBOARD 0
+#define NVBOARD 1
 
 VerilatedContext* contextp = NULL;
 VerilatedVcdC* tfp = NULL;
