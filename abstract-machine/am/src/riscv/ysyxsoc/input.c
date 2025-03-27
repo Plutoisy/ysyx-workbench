587eb1e2ea1b46fb055d1654c4b4e8cb5b9829e2
tracer-ysyx <tracer@ysyx.org>
2025-03-27 17:21:12 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:21:12 up 16:46,  1 user,  load average: 0.81, 0.70, 0.62

--- abstract-machine/am/src/riscv/ysyxsoc/input.c
+++ abstract-machine/am/src/riscv/ysyxsoc/input.c
@@ -7,7 +7,7 @@
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t kbd_out = inl(KBD_ADDR) ;
   kbd->keydown = (kbd_out & KEYDOWN_MASK ? true : false);
-  kbd->keycode = kbd_out & ~KEYDOWN_MASK;
+  kbd->keycode = kbd_out;
 }
 
 void __am_uart_rx(AM_UART_RX_T *rx) {
