d172babe2f2396bd5c6f8383bd54a0174b09a3b3
211180120-Yu Bingyao <211180120@smail.nju.edu.cn>
2025-03-27 14:03:01 +0800

    uart rx

---
+++ abstract-machine/am/src/riscv/ysyxsoc/soc.h
@@ -0,0 +1,8 @@
+#define UART_BASE 0x10000000L
+#define UART_TX   0
+#define UART_RX   0
+#define SERIAL_PORT     (UART_BASE + UART_TX)
+#define UART_REG_LC      0x10000003L
+#define UART_REG_DL1     0x10000000L
+#define UART_REG_DL2     0x10000001L
+#define UART_REG_LSR     0x10000005L
