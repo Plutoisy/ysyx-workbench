8e1cd67e95beb2a4df72cd2ac793f06d83304f69
tracer-ysyx <tracer@ysyx.org>
2025-03-28 14:24:25 +0800

    > compile NEMU ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:24:25 up 1 day,  1:12,  1 user,  load average: 0.05, 0.16, 0.22

---
+++ abstract-machine/am/src/riscv/ysyxsoc/gpu.c
@@ -0,0 +1,27 @@
+#include <am.h>
+
+const int disp_w = 400, disp_h = 300;
+
+void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+//   uint32_t *pixels = ctl->pixels;
+//   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+//   for (int i = 1; i <= ctl->h; i ++){
+//     for (int j = 1; j <= ctl->w; j ++){
+//       fb[disp_w*(i+(ctl->y)-1)+j+(ctl->x)-1] = pixels[ctl->w*(i-1)+j-1];
+//     }
+//   }
+//   if (ctl->sync) {
+//     outl(SYNC_ADDR, 1);
+//   }
+}
+
+void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  *cfg = (AM_GPU_CONFIG_T) {
+    .present = true, .has_accel = false,
+    .width = disp_w, .height = disp_h,
+    .vmemsz = 0
+  };
+}
+void __am_gpu_status(AM_GPU_STATUS_T *status) {
+  status->ready = true;
+}
