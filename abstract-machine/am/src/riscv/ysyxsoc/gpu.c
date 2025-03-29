68e10d3065fefe5d702110601143eb60dd7a5eb5
tracer-ysyx <tracer@ysyx.org>
2025-03-29 12:44:38 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:44:38 up 1 day,  3:31,  1 user,  load average: 1.54, 1.09, 0.74

--- abstract-machine/am/src/riscv/ysyxsoc/gpu.c
+++ abstract-machine/am/src/riscv/ysyxsoc/gpu.c
@@ -1,63 +1,32 @@
 #include <am.h>
 #include "soc.h"
 #include <riscv/riscv.h>
-#include <stdio.h>
-#include <string.h>
 
-static uint32_t Width = 640;
-static uint32_t Height = 480;
+const int disp_w = 640, disp_h = 480;
 
-void __am_gpu_init() {
-  // int i;
-
-  // uint32_t config = inl(VGACTL_ADDR);
-  // Width = (config >> 16) & 0x0000ffff;
-  // Height = config & 0x0000ffff;
-  // printf("%d %d\n",Width,Height);
+void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t *pixels = ctl->pixels;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (int i = 0; i < Width * Height; i ++){
-    fb[i] = 0;
-  } 
-  outl(SYNC_ADDR, 1);
+  for (int i = 1; i <= ctl->h; i ++){
+    for (int j = 1; j <= ctl->w; j ++){
+      fb[disp_w*(i+(ctl->y)-1)+j+(ctl->x)-1] = pixels[ctl->w*(i-1)+j-1];
+    }
+  }
+//   if (ctl->sync) {
+//     outl(SYNC_ADDR, 1);
+//   }
+//   else{
+//     outl(SYNC_ADDR, 0);
+//   }
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  // uint32_t config = inl(VGACTL_ADDR);
-  // int w = (config >> 16) & 0x0000ffff;
-  // int h = config & 0x0000ffff;
-
   *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, 
-    .has_accel = false,
-    .width = Width, 
-    .height = Height,
-    .vmemsz = Width * Height * sizeof(uint32_t)
+    .present = true, .has_accel = false,
+    .width = disp_w, .height = disp_h,
+    .vmemsz = 0
   };
 }
-
-void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  uint32_t x = ctl->x, y = ctl->y;
-  uint32_t w = ctl->w, h = ctl->h;
-  // uint32_t *pixels = (uint32_t *)ctl->pixels;
-  // uint32_t *fb = (uint32_t *)FB_ADDR;
-  // memcpy(fb + ((y * Width) + x),pixels,w*h*4);
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
-  }else{
-    outl(SYNC_ADDR, 0);
-  }
-  if(w==0 || h==0) return;
-  uint32_t *pixels = (uint32_t *)ctl->pixels;
-  uint32_t *fb = (uint32_t *)FB_ADDR;
-  // memset(fb + (y*Width + x), (int)pixels, h*w*4);
-  for (int i = 0; i < h; i ++){
-    for(int j = 0; j < w; j++){
-      fb[(i+y)*Width + x + j] = pixels[i*w + j];
-    }
-  }
- 
-}
-
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
   status->ready = true;
 }
