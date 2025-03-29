eab830fb022fdacd0a5e926f0a0709ddf04d3d6b
tracer-ysyx <tracer@ysyx.org>
2025-03-29 12:29:10 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:29:10 up 1 day,  3:15,  1 user,  load average: 0.79, 0.64, 0.42

---
+++ npc/obj_dir/VysyxSoCFull___024unit__DepSet_h308b0958__0.cpp
@@ -0,0 +1,67 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See VysyxSoCFull.h for the primary calling header
+
+#include "verilated.h"
+#include "verilated_dpi.h"
+
+#include "VysyxSoCFull__Syms.h"
+#include "VysyxSoCFull___024unit.h"
+
+extern "C" void reg_out(const int* array);
+
+VL_INLINE_OPT void VysyxSoCFull___024unit____Vdpiimwrap_reg_out_TOP____024unit(VlUnpacked<IData/*31:0*/, 32> array) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+        VysyxSoCFull___024unit____Vdpiimwrap_reg_out_TOP____024unit\n"); );
+    // Body
+    int array__Vcvt[32];
+    for (size_t array__Vidx = 0; array__Vidx < 32; ++array__Vidx) array__Vcvt[array__Vidx] = (&array[0])[array__Vidx];
+    reg_out(array__Vcvt);
+}
+
+extern "C" void ebreak();
+
+VL_INLINE_OPT void VysyxSoCFull___024unit____Vdpiimwrap_ebreak_TOP____024unit() {
+    VL_DEBUG_IF(VL_DBG_MSGF("+        VysyxSoCFull___024unit____Vdpiimwrap_ebreak_TOP____024unit\n"); );
+    // Body
+    ebreak();
+}
+
+extern "C" void npc_trap(int pc, int ret);
+
+VL_INLINE_OPT void VysyxSoCFull___024unit____Vdpiimwrap_npc_trap_TOP____024unit(IData/*31:0*/ pc, IData/*31:0*/ ret) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+        VysyxSoCFull___024unit____Vdpiimwrap_npc_trap_TOP____024unit\n"); );
+    // Body
+    int pc__Vcvt;
+    for (size_t pc__Vidx = 0; pc__Vidx < 1; ++pc__Vidx) pc__Vcvt = pc;
+    int ret__Vcvt;
+    for (size_t ret__Vidx = 0; ret__Vidx < 1; ++ret__Vidx) ret__Vcvt = ret;
+    npc_trap(pc__Vcvt, ret__Vcvt);
+}
+
+extern "C" void get_pc_inst(int pc, int dnpc, int inst, int IFU_valid_int);
+
+VL_INLINE_OPT void VysyxSoCFull___024unit____Vdpiimwrap_get_pc_inst_TOP____024unit(IData/*31:0*/ pc, IData/*31:0*/ dnpc, IData/*31:0*/ inst, IData/*31:0*/ IFU_valid_int) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+        VysyxSoCFull___024unit____Vdpiimwrap_get_pc_inst_TOP____024unit\n"); );
+    // Body
+    int pc__Vcvt;
+    for (size_t pc__Vidx = 0; pc__Vidx < 1; ++pc__Vidx) pc__Vcvt = pc;
+    int dnpc__Vcvt;
+    for (size_t dnpc__Vidx = 0; dnpc__Vidx < 1; ++dnpc__Vidx) dnpc__Vcvt = dnpc;
+    int inst__Vcvt;
+    for (size_t inst__Vidx = 0; inst__Vidx < 1; ++inst__Vidx) inst__Vcvt = inst;
+    int IFU_valid_int__Vcvt;
+    for (size_t IFU_valid_int__Vidx = 0; IFU_valid_int__Vidx < 1; ++IFU_valid_int__Vidx) IFU_valid_int__Vcvt = IFU_valid_int;
+    get_pc_inst(pc__Vcvt, dnpc__Vcvt, inst__Vcvt, IFU_valid_int__Vcvt);
+}
+
+extern "C" void flash_read(int addr, int* data);
+
+VL_INLINE_OPT void VysyxSoCFull___024unit____Vdpiimwrap_flash_read_TOP____024unit(IData/*31:0*/ addr, IData/*31:0*/ &data) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+        VysyxSoCFull___024unit____Vdpiimwrap_flash_read_TOP____024unit\n"); );
+    // Body
+    int addr__Vcvt;
+    for (size_t addr__Vidx = 0; addr__Vidx < 1; ++addr__Vidx) addr__Vcvt = addr;
+    int data__Vcvt;
+    flash_read(addr__Vcvt, &data__Vcvt);
+    data = data__Vcvt;
+}
