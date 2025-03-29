b23ad014e59ae1657402bcd9816acbf7c3e76916
tracer-ysyx <tracer@ysyx.org>
2025-03-29 12:13:31 +0800

    > compile NEMU ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:13:31 up 1 day,  2:59,  1 user,  load average: 0.73, 0.37, 0.24

--- abstract-machine/am/src/riscv/ysyxsoc/gpu.c
+++ abstract-machine/am/src/riscv/ysyxsoc/gpu.c
@@ -1,17 +1,21 @@
 #include <am.h>
+#include "soc.h"
 
 const int disp_w = 640, disp_h = 480;
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-//   uint32_t *pixels = ctl->pixels;
-//   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-//   for (int i = 1; i <= ctl->h; i ++){
-//     for (int j = 1; j <= ctl->w; j ++){
-//       fb[disp_w*(i+(ctl->y)-1)+j+(ctl->x)-1] = pixels[ctl->w*(i-1)+j-1];
-//     }
-//   }
+  uint32_t *pixels = ctl->pixels;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (int i = 1; i <= ctl->h; i ++){
+    for (int j = 1; j <= ctl->w; j ++){
+      fb[disp_w*(i+(ctl->y)-1)+j+(ctl->x)-1] = pixels[ctl->w*(i-1)+j-1];
+    }
+  }
 //   if (ctl->sync) {
 //     outl(SYNC_ADDR, 1);
+//   }
+//   else{
+//     outl(SYNC_ADDR, 0);
 //   }
 }
 
