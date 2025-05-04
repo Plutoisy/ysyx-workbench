9d7265060633bd81b0c3dfc2fcf044f895187c11
tracer-ysyx <tracer@ysyx.org>
2025-05-04 21:49:00 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-58-generic #60~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 28 16:09:21 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:49:00 up 1 day,  3:38,  1 user,  load average: 0.55, 0.58, 0.74

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -340,28 +340,28 @@ extern "C" void Performance_Counters(int Performancetype){
   }
 }
 
-uint64_t jump_type = 0;
-uint64_t csr_type = 0;
-uint64_t read_and_store_type = 0;
-uint64_t cal_type = 0;
-uint64_t unk = 0;
-extern "C" void inst_type_Counters(int insttype){
-  if(insttype == 1){
-    jump_type++;
-  }
-  if(insttype == 2){
-    csr_type++;
-  }
-  if(insttype == 3){
-    read_and_store_type++;
-  }
-  if(insttype == 4){
-    cal_type++;
-  }
-  if(insttype == 5){
-    unk++;
-  }
-}
+// uint64_t jump_type = 0;
+// uint64_t csr_type = 0;
+// uint64_t read_and_store_type = 0;
+// uint64_t cal_type = 0;
+// uint64_t unk = 0;
+// extern "C" void inst_type_Counters(int insttype){
+//   if(insttype == 1){
+//     jump_type++;
+//   }
+//   if(insttype == 2){
+//     csr_type++;
+//   }
+//   if(insttype == 3){
+//     read_and_store_type++;
+//   }
+//   if(insttype == 4){
+//     cal_type++;
+//   }
+//   if(insttype == 5){
+//     unk++;
+//   }
+// }
 
 extern "C" void psram_read(uint32_t addr, uint32_t *data) {
 	if(addr >= 0 && addr <= PSRAM_SIZE){
