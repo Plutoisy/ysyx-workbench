99fa18a1c7ec440e54e967ad4d5d8e115da8ae58
tracer-ysyx <tracer@ysyx.org>
2025-05-17 22:36:00 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:36:00 up  7:53,  1 user,  load average: 1.38, 0.78, 0.64

--- npc/npc_specific/ysyx_24120011_Xbar.v
+++ npc/npc_specific/ysyx_24120011_Xbar.v
@@ -718,7 +718,7 @@ module ysyx_24120011_Xbar(
         case(r_Xbar_state)
             ysyx_24120011_Xbar_IDLE:begin
                 if(Xbar_arvalid) begin
-                    if(Xbar_araddr == 32'h02000048 || Xbar_araddr == 32'h0200004c) begin
+                    if(Xbar_araddr == 32'ha0000048 || Xbar_araddr == 32'ha000004c) begin
                         r_Xbar_next_state = ysyx_24120011_Xbar_S2;
                     end
                     else begin
