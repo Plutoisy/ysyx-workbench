a03a0393f59436feabb1f9b60c9ae1ad13ad3c86
211180120-Yu Bingyao <211180120@smail.nju.edu.cn>
2025-05-18 16:13:17 +0800

    保存perf

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -27,5 +27,4 @@ image: $(IMAGE).elf
 run: image
 	$(MAKE) -C $(NPC_HOME) nvboard IMG=$(IMAGE).bin
     # $(MAKE) -C $(NPC_HOME) nvboard_bs IMG=$(IMAGE).bin
-    # $(MAKE) -C $(NPC_HOME) nvboard IMG=$(IMAGE).bin
-    # $(MAKE) -C $(NPC_HOME) nvboard IMG=$(IMAGE).bin
+    # $(MAKE) -C $(NPC_HOME) nvboard IMG=$(IMAGE).bin\
