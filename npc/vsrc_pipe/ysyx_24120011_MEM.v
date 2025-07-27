70de93648309c7a36e589dc0c0d53a2c89e0f279
tracer-ysyx <tracer@ysyx.org>
2025-07-27 17:15:11 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:15:11 up 6 days,  5:07,  1 user,  load average: 0.06, 0.08, 0.29

--- npc/vsrc_pipe/ysyx_24120011_MEM.v
+++ npc/vsrc_pipe/ysyx_24120011_MEM.v
@@ -88,7 +88,7 @@ reg [31:0] r_mem_data;
 
 //assign o_MEM_ready  = LSU_ready ;
 assign o_MEM_ready  = ((state == ysyx_24120011_LSU_M_AXI_IDLE) && i_MEMWB_ready) ? 1'b1 : 1'b0;
-assign o_MEM_valid  = (state == ysyx_24120011_LSU_M_AXI_RWCHECK) && (next_state == ysyx_24120011_LSU_M_AXI_IDLE) ? 1'b1 : (LSU_working == 1 && next_state == ysyx_24120011_LSU_M_AXI_IDLE ? 1'b1 : 1'b0);
+assign o_MEM_valid  = (state != ysyx_24120011_LSU_M_AXI_IDLE) && (next_state == ysyx_24120011_LSU_M_AXI_IDLE) ? 1'b1 : 1'b0;
 assign o_r_mem_data = r_mem_data;
 assign o_rd_ctrl    = rd_ctrl   ;
 assign o_csr_ctrl   = csr_ctrl  ;
@@ -269,14 +269,15 @@ always @(posedge clk) begin
         LSU_working_delay <= LSU_working;
 
         if (state == ysyx_24120011_LSU_M_AXI_IDLE) begin
+            LSU_clktime_count(cycle_counter);
             cycle_counter <= 0;
         end
         else begin
             cycle_counter <= cycle_counter + 1'b1;
         end
-        if (~LSU_working & LSU_working_delay) begin//LSU_working下降沿
-            LSU_clktime_count(cycle_counter);
-        end
+        // if (~LSU_working & LSU_working_delay) begin//LSU_working下降沿
+        //     LSU_clktime_count(cycle_counter);
+        // end
     end
 end
 //======================dpic========================//
