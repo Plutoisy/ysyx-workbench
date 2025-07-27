d9b68c4e74bee282c9f6bfef195c0ace9388856d
tracer-ysyx <tracer@ysyx.org>
2025-07-27 23:14:36 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:14:36 up 6 days, 11:07,  1 user,  load average: 1.33, 0.88, 0.55

--- npc/obj_dir/VysyxSoCFull__Dpi.h
+++ npc/obj_dir/VysyxSoCFull__Dpi.h
@@ -16,29 +16,27 @@ extern "C" {
 
 
     // DPI IMPORTS
-    // DPI import at /home/plutoisy/ysyx-workbench/npc/vsrc_pipe/ysyx_24120011_IFU.v:1:30
+    // DPI import at /home/plutoisy/ysyx-workbench/npc/vsrc/ysyx_24120011_IFU.v:1:30
     extern void IFU_clktime_count(int ifu_clk_count, int hit);
-    // DPI import at /home/plutoisy/ysyx-workbench/npc/vsrc_pipe/ysyx_24120011_MEM.v:1:30
+    // DPI import at /home/plutoisy/ysyx-workbench/npc/vsrc/ysyx_24120011_LSU.v:1:30
     extern void LSU_clktime_count(int lsu_clk_count);
-    // DPI import at /home/plutoisy/ysyx-workbench/npc/vsrc_pipe/ysyx_24120011.v:4:30
+    // DPI import at /home/plutoisy/ysyx-workbench/npc/vsrc/ysyx_24120011.v:5:30
     extern void Performance_Counters(int Performancetype);
-    // DPI import at /home/plutoisy/ysyx-workbench/npc/vsrc_pipe/ysyx_24120011.v:1:30
+    // DPI import at /home/plutoisy/ysyx-workbench/npc/vsrc/ysyx_24120011.v:2:30
     extern void ebreak();
     // DPI import at /home/plutoisy/ysyx-workbench/ysyxSoC/perip/flash/flash.v:84:30
     extern void flash_read(int addr, int* data);
-    // DPI import at /home/plutoisy/ysyx-workbench/npc/vsrc_pipe/ysyx_24120011.v:3:30
+    // DPI import at /home/plutoisy/ysyx-workbench/npc/vsrc/ysyx_24120011.v:4:30
     extern void get_pc_inst(int pc, int dnpc, int inst, int IFU_valid_int);
-    // DPI import at /home/plutoisy/ysyx-workbench/npc/vsrc_pipe/ysyx_24120011_IFU.v:2:30
-    extern void icahce_miss_count(int miss_count);
-    // DPI import at /home/plutoisy/ysyx-workbench/ysyxSoC/build/ysyxSoCFull.v:53163:30
+    // DPI import at /home/plutoisy/ysyx-workbench/ysyxSoC/build/ysyxSoCFull.v:4627:30
     extern void mrom_read(int raddr, int* rdata);
-    // DPI import at /home/plutoisy/ysyx-workbench/npc/vsrc_pipe/ysyx_24120011.v:2:30
+    // DPI import at /home/plutoisy/ysyx-workbench/npc/vsrc/ysyx_24120011.v:3:30
     extern void npc_trap(int pc, int ret);
     // DPI import at /home/plutoisy/ysyx-workbench/ysyxSoC/perip/psram/psram.v:161:32
     extern void psram_read(int addr, int* data);
     // DPI import at /home/plutoisy/ysyx-workbench/ysyxSoC/perip/psram/psram.v:162:32
     extern void psram_write(int addr, int data, int mask);
-    // DPI import at /home/plutoisy/ysyx-workbench/npc/vsrc_pipe/ysyx_24120011_GPR.v:2:30
+    // DPI import at /home/plutoisy/ysyx-workbench/npc/vsrc/ysyx_24120011_RegStack.v:2:30
     extern void reg_out(const int* array);
 
 #ifdef __cplusplus
