3c57c6197f16342f76a5292a59ea2da757dd8fb2
tracer-ysyx <tracer@ysyx.org>
2025-08-01 23:23:05 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:23:05 up 14:44,  1 user,  load average: 0.82, 0.76, 0.55

--- npc/obj_dir/VysyxSoCFull___024root__DepSet_h4d156741__5.cpp
+++ npc/obj_dir/VysyxSoCFull___024root__DepSet_h4d156741__5.cpp
@@ -20853,7 +20853,7 @@ VL_INLINE_OPT void VysyxSoCFull___024root___nba_sequent__TOP__5(VysyxSoCFull___0
                                                   >> 0x14U)))))
                             : ((IData)(4U) + vlSelfRef.ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__u_ysyx_24120011_IFU__DOT__pc)));
             }
-        } else {
+        } else if ((0U == (IData)(vlSelfRef.ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__u_ysyx_24120011_IFU__DOT__state))) {
             vlSelfRef.__Vdly__ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__u_ysyx_24120011_IFU__DOT__pc 
                 = vlSelfRef.ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__u_ysyx_24120011_EXU__DOT__npc;
         }
