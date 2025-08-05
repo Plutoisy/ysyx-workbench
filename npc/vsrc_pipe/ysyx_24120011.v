6d7d3b267bdfe6a296c9571d5de3f59fa357ca3b
tracer-ysyx <tracer@ysyx.org>
2025-08-05 21:44:13 +0800

    > sim RTL ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:44:13 up 2 days, 21:22,  1 user,  load average: 0.70, 0.46, 0.45

--- npc/vsrc_pipe/ysyx_24120011.v
+++ npc/vsrc_pipe/ysyx_24120011.v
@@ -742,30 +742,16 @@ wire DATAHAZARD_stop_pipe;
 wire [31:0] DATAHAZARD_IDU_r_ddata;
 wire DATAHAZARD_IDU_rs1_or_rs2;
 wire DATAHAZARD_IDU_bypass;
+
 ysyx_24120011_data_hazard_detection u_ysyx_24120011_data_hazard_detection(
     .i_IDU_rs1     ( IDU_GPR_rs1     ),
     .i_IDU_rs2     ( IDU_GPR_rs2     ),
-    //.i_IDEX_rd     ( IDEX_EXU_rd     ),
     .i_EXU_rd      ( EXU_MEM_rd      ),
-    .i_EXU_rd_ctrl ( EXU_MEM_rd_ctrl),
-    //.i_EXMEM_rd    ( EXMEM_MEM_rd    ),
+    .i_EXU_rd_data_type ( EXU_MEM_rd_data_type),
     .i_MEM_rd      ( MEM_WBU_rd      ),
-    .i_MEM_rd_ctrl ( MEM_WBU_rd_ctrl),
-    //.i_MEMWB_rd    ( MEMWB_WBU_rd    ),
-    //.i_WBU_rd      ( WBU_GPR_rd      ),
-    //.i_WBU_rd_ctrl ( WBU_rd_ctrl),
-    //.i_WBU_rd_data ( WBU_GPR_rd_data),
-    //.i_GPR_rd      (WBU_GPR_rd),
-    //.i_IDEX_ready  ( IDEX_ready  ),
+    .i_MEM_rd_data_en ( MEM_WBU_rd_data_en),
     .i_EXU_ready   ( EXU_ready   ),
-    //.i_EXMEM_ready ( EXMEM_ready ),
-
     .i_MEM_ready   ( MEM_ready   ),
-    //.i_MEMWB_ready ( MEMWB_ready ),
-    //.i_WBU_ready   ( WBU_ready   ),
-
-
-    //.i_GPR_en      ( WBU_GPR_rd_en      ),
     .o_stop_pipe   ( DATAHAZARD_stop_pipe   ),
     .o_rd_data     ( DATAHAZARD_IDU_r_ddata),
     .o_rs1_or_rs2  ( DATAHAZARD_IDU_rs1_or_rs2),
