6a13a1e233259249d7c9df45d0123386375c411e
tracer-ysyx <tracer@ysyx.org>
2025-09-02 20:55:07 +0800

    > compile NEMU ysyx_22040000 喻炳尧 Linux plutoisy-ubuntu 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:55:07 up 4 days, 22:54,  1 user,  load average: 0.80, 0.51, 0.30

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -20,11 +20,11 @@
 #define SCREEN_H (MUXDEF(CONFIG_VGA_SIZE_800x600, 600, 300))
 
 static uint32_t screen_width() {
-  return MUXDEF(CONFIG_TARGET_AM, io_read(AM_GPU_CONFIG).width, SCREEN_W);
+  return SCREEN_W;
 }
 
 static uint32_t screen_height() {
-  return MUXDEF(CONFIG_TARGET_AM, io_read(AM_GPU_CONFIG).height, SCREEN_H);
+  return SCREEN_H;
 }
 
 static uint32_t screen_size() {
