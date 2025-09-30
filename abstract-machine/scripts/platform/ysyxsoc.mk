a16c5090ba76d7d3a12f7b8242e4a8ef1f892c08
tracer-ysyx <tracer@ysyx.org>
2025-09-30 09:39:14 +0800

    > sim RTL ysyx_24120011 喻炳尧 Linux plutoisy-ubuntu 6.8.0-79-generic #79~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 15 16:54:53 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:39:14 up 3 days,  1:33,  1 user,  load average: 0.33, 0.31, 0.43

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -19,12 +19,15 @@ LDFLAGS   += -Map out.map
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 .PHONY: $(AM_HOME)/am/src/riscv/ysyxsoc/trm.c
 
-image: $(IMAGE).elf
+insert-arg: image
+	@python $(AM_HOME)/tools/insert-arg.py $(IMAGE).bin $(MAINARGS_MAX_LEN) "$(MAINARGS_PLACEHOLDER)" "$(mainargs)"
+	
+image: image-dep
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
-run: image
+run: insert-arg
 	$(MAKE) -C $(NPC_HOME) nvboard IMG=$(IMAGE).bin
     # $(MAKE) -C $(NPC_HOME) nvboard_bs IMG=$(IMAGE).bin
     # $(MAKE) -C $(NPC_HOME) nvboard IMG=$(IMAGE).bin\
