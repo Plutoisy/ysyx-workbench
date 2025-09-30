545ac38d9016c8923d5878cd8ef8d0bb69c13ee1
tracer-ysyx <tracer@ysyx.org>
2025-09-30 18:16:35 +0800

    > sim RTL ysyx_24120011 喻炳尧 Linux plutoisy-ubuntu 6.8.0-84-generic #84~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 14:29:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:16:35 up  8:32,  1 user,  load average: 0.07, 0.19, 0.25

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -81,12 +81,17 @@ void halt(int code) {
 }
 
 void uart_init() {
-  outb(UART_BASE + 3, 0b10000011);
-  outb(UART_BASE + 1, 0);
-  outb(UART_BASE, 1);
-  outb(UART_BASE + 3, 0b00000011);
-  outb(UART_BASE + 2, 0b11000110);
-  outb(UART_BASE + 1, 0b00000000);
+  uint16_t divisor = 32;  // 修改成 16
+  // 打开 DLAB
+  outb(UART_REG_LC, inb(UART_REG_LC) | 0x80);
+  // 写高低字节
+  outb(UART_REG_DL2, (divisor >> 8) & 0xFF);
+  outb(UART_REG_DL1, divisor & 0xFF);
+  // 关闭 DLAB
+  outb(UART_REG_LC, inb(UART_REG_LC) & ~0x80);
+
+  // 设置 8N1 格式
+  outb(UART_REG_LC, 0x03); // 8 bit, no parity, 1 stop bit
 }
 
 
