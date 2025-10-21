4b895419ecb3074b4ccb3104b3fc4b39daf64855
tracer-ysyx <tracer@ysyx.org>
2025-10-21 15:29:36 +0800

    > sim RTL ysyx_24120011 喻炳尧 Linux plutoisy-ubuntu 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:29:36 up 13:13,  1 user,  load average: 0.77, 0.83, 0.89

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -235,13 +235,13 @@ static long load_img_to_flash(char *img) {
 
 int nvboard_update_count = 0;
 void step_and_dump_wave(){
-  if(NVBOARD && nvboard_update_count == 0){
+//  if(NVBOARD && nvboard_update_count == 0){
     nvboard_update();
-    nvboard_update_count = 1;
-  }
-  else if(NVBOARD && nvboard_update_count == 1){
-    nvboard_update_count = 0;
-  }
+//    nvboard_update_count = 1;
+  // }
+  // else if(NVBOARD && nvboard_update_count == 1){
+  //   nvboard_update_count = 0;
+  // }
   dut.eval();
   if(WAVE){
     //if(((top_pc & 0xFF000000) >> 24) == 0xA0){
