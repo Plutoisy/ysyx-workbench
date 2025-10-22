3b9d3103454f4220136c2c1a8c30f26f68d5af98
tracer-ysyx <tracer@ysyx.org>
2025-10-22 21:50:31 +0800

    > sim RTL ysyx_24120011 喻炳尧 Linux plutoisy-ubuntu 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:50:31 up  7:27,  1 user,  load average: 0.05, 0.17, 0.30

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -744,8 +744,8 @@ void cpu_exec(uint64_t n){
 
         if(DIFFTESE){
           printf("        dut                    | ref                   \n");
-          printf("pc      0x%08x             | 0x%08x\n", top_dnpc, refstate.pc);
-          if(refstate.pc != top_dnpc){
+          printf("pc      0x%08x             | 0x%08x\n", top_npc, refstate.pc);
+          if(refstate.pc != top_npc){
            //AssembleDecoder(handle, top_inst, top_pc);
            if(PC_ASSERT){
              assert(0);
