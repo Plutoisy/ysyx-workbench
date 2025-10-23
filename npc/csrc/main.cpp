46f18df1943d813b70659a6f7221f962dd7c667c
tracer-ysyx <tracer@ysyx.org>
2025-10-23 11:38:49 +0800

    > sim RTL ysyx_24120011 喻炳尧 Linux plutoisy-ubuntu 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:38:49 up 11:13,  1 user,  load average: 0.25, 0.65, 0.57

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -786,7 +786,7 @@ void cpu_exec(uint64_t n){
           if(detect_read_device){
             dutstate.pc = top_pc;
             memcpy(dutstate.gpr, gpr, sizeof(gpr));
-            difftest_regcpy(&dutstate, 1, top_inst);
+            difftest_regcpy(&dutstate, 1, top_pc);
             difftest_regcpy(&refstate, 0, 0x30000000);
           }
           else{
