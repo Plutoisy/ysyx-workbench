727714e87eba5702a52d5e57a62ae0bd658c0fbc
tracer-ysyx <tracer@ysyx.org>
2025-10-24 22:22:47 +0800

    > sim RTL ysyx_24120011 喻炳尧 Linux plutoisy-ubuntu 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:22:47 up 22:53,  1 user,  load average: 2.20, 1.92, 1.32

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -769,7 +769,7 @@ void cpu_exec(uint64_t n){
           // if(top_pc == 0xa0000518){
           //   return;
           // }
-          if(top_pc == 0xa002cd28){
+          if(top_pc == 0xa00214ac){
             return;
           }
           
