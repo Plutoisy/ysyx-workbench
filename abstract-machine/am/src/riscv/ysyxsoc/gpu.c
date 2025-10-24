425dd09a3de949d2fd212e8c897f0c2c09be2c72
tracer-ysyx <tracer@ysyx.org>
2025-10-24 21:52:38 +0800

    > sim RTL ysyx_24120011 喻炳尧 Linux plutoisy-ubuntu 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:52:38 up 22:23,  1 user,  load average: 0.58, 1.16, 1.67

--- abstract-machine/am/src/riscv/ysyxsoc/gpu.c
+++ abstract-machine/am/src/riscv/ysyxsoc/gpu.c
@@ -8,16 +8,16 @@ const int disp_w = 640, disp_h = 480;
 #define HEIGHT     1024
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  uint32_t x = ctl->x, y = ctl->y;
-  uint32_t w = ctl->w, h = ctl->h;
-  uint32_t *pixels = (uint32_t *)ctl->pixels;
+  // uint32_t x = ctl->x, y = ctl->y;
+  // uint32_t w = ctl->w, h = ctl->h;
+  // uint32_t *pixels = (uint32_t *)ctl->pixels;
   
-  for (int i = 0; i < w; i++) {
-    for (int j = 0; j < h; j++) {
-        uintptr_t addr = FB_ADDR + (((i+x) * 2048 + (j+y)) * 4);
-        outl(addr, pixels[i*h + j]);
-    }
-  }
+  // for (int i = 0; i < w; i++) {
+  //   for (int j = 0; j < h; j++) {
+  //       uintptr_t addr = FB_ADDR + (((i+x) * 2048 + (j+y)) * 4);
+  //       outl(addr, pixels[i*h + j]);
+  //   }
+  // }
 //   for (int i = 1; i <= h; i ++){
 //     for (int j = 1; j <= w; j ++){
 //       fb[disp_w*(i+y-1)+j+x-1] = pixels[w*(i-1)+j-1];
