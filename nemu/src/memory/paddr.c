9b52741e67654d20c7c4c276dc29cd41115ce6c7
tracer-ysyx <tracer@ysyx.org>
2025-10-25 23:19:04 +0800

    > compile NEMU ysyx_24120011 喻炳尧 Linux plutoisy-ubuntu 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:19:04 up 1 day,  8:05,  1 user,  load average: 0.44, 0.26, 0.27

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -97,7 +97,7 @@ void init_mem() {
 
 word_t paddr_read(paddr_t addr, int len) {
   IFDEF(CONFIG_MTRACE, printf("R->addr: 0x%x, len: %d, mem: 0x%08x\n", addr, len, pmem_read(addr,len)));
-  if(CONFIG_SOC_DIFF){
+  if(CONFIG_PADDR_MODE == 1){
     if (likely(in_flash(addr))) return flash_read(addr, len);
     if (likely(in_sram(addr)))  return sram_read(addr, len);
     if (likely(in_sdram(addr))) return sdram_read(addr, len);
@@ -115,7 +115,7 @@ word_t paddr_read(paddr_t addr, int len) {
 
 void paddr_write(paddr_t addr, int len, word_t data) {
   IFDEF(CONFIG_MTRACE, printf("W->addr: 0x%x, len: %d, mem: 0x%08x\n", addr, len, data));
-  if(CONFIG_SOC_DIFF){
+  if(CONFIG_PADDR_MODE == 1){
     if (likely(in_flash(addr))) { flash_write(addr, len, data); return; }
     if (likely(in_sram(addr)))  { sram_write(addr, len, data); return; }
     if (likely(in_sdram(addr))) { sdram_write(addr, len, data); return; }
