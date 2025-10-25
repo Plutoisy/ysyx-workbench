2060a4b57010a929be5732f9bcf6744b9eb0e785
tracer-ysyx <tracer@ysyx.org>
2025-10-25 16:04:47 +0800

    > compile NEMU ysyx_24120011 喻炳尧 Linux plutoisy-ubuntu 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:04:47 up 1 day,  2:10,  1 user,  load average: 0.21, 0.32, 0.35

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -10,10 +10,5 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
 }
 
 void __am_uart_rx(AM_UART_RX_T *rx) {
-  AM_INPUT_KEYBRD_T kbd;
-  __am_input_keybrd(&kbd);
-  if(kbd.keydown){
-    printf("%x",kbd.keycode);
-  }
   rx->data = 0xff;
 }
