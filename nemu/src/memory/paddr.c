2cc07fb0b651292376d8d871cbd854426b23de9f
tracer-ysyx <tracer@ysyx.org>
2025-10-25 14:02:21 +0800

    > compile NEMU ysyx_24120011 喻炳尧 Linux plutoisy-ubuntu 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:02:21 up 1 day, 8 min,  1 user,  load average: 0.54, 0.42, 0.55

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -110,6 +110,7 @@ word_t paddr_read(paddr_t addr, int len) {
   else{
     if (likely(in_pmem(addr))) return pmem_read(addr, len);
     IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
+    printf("fuck\n");
     out_of_bound(addr);
     return 0;
   }
@@ -127,7 +128,6 @@ void paddr_write(paddr_t addr, int len, word_t data) {
   else{
     if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
     IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
-    printf("fuck\n");
     out_of_bound(addr);
   }
 }
