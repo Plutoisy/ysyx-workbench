c9b0acc2ec6204abf9e7a0c48db3a854c03401e6
tracer-ysyx <tracer@ysyx.org>
2025-10-26 22:09:41 +0800

    > compile NEMU ysyx_24120011 喻炳尧 Linux plutoisy-ubuntu 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:09:41 up 1 day, 12:16,  1 user,  load average: 0.00, 0.16, 0.43

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -3,6 +3,7 @@
 # Add necessary options if the target is a shared library
 ifeq ($(SHARE),1)
 DOTSO = .so
+$(info VAR is $(PADDR_MODE))
 ifeq ($(PADDR_MODE), 1)
     DOTSO = -soc.so
 else ifeq ($(PADDR_MODE), 2)
